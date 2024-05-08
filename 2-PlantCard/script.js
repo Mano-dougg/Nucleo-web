b84ddbea12914eb5b682852256bc98dedff98846
Marcello Lima da Conceição <marcelloconceicao@ufba.br>
2024-05-08 15:28:39 -0300

    Atividade finalizada, bug com <object> resolvido, adicionada animação e debug do script

--- 2-PlantCard/script.js
+++ 2-PlantCard/script.js
@@ -1,3 +1,47 @@
-function triggerAnimation () {
-    
+let share_btn = document.getElementById("share");
+let curtain = document.getElementById("curtain");
+let counter = 0;
+
+
+function inAnim () {
+    curtain.classList.remove("OutAnimation");
+    curtain.classList.add("InAnimation");
+ 
+}; 
+
+function outAnim () {
+    curtain.classList.remove("InAnimation");
+    curtain.classList.add("OutAnimation");
+
+}
+
+function Anim() {
+    if (counter % 2 == 0) {
+        inAnim();
+        counter++;
+    } else {
+        outAnim();
+        counter++;
+    }
+}
+
+share_btn.addEventListener("click" , Anim);
+
+document.getElementsByTagName("img")[1].onclick = function() {
+    location.href = "https://www.facebook.com";
+
 };
+
+document.getElementsByTagName("img")[2].onclick = function() {
+    location.href = "https://www.twitter.com";
+
+};
+
+document.getElementsByTagName("img")[3].onclick = function() {
+    location.href = "https://www.pinterest.com";
+
+};
+
+
+
+
