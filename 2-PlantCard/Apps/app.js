6f32921bcb6f32138097dbc4dc8e83b0cf05941e
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-09 15:10:40 -0300

    fix: Melhorada acessibilidade.

--- 2-PlantCard/Apps/app.js
+++ 2-PlantCard/Apps/app.js
@@ -1,8 +1,14 @@
 const botao = document.getElementById('botaoShare');
 
-function abreMenu() {
+function abreMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
     const share = document.getElementById('compartilhar');
     share.classList.toggle('ativo');
+    const ativou = share.classList.contains('ativo');
+    event.currentTarget.setAttribute('aria-expanded', ativou);
+    if(ativou) { event.currentTarget.setAttribute('aria-label', '"fecha opções de compartilhamento"');
+} else { event.currentTarget.setAttribute('aria-label', '"abre opções de compartilhamento"') }
 }
 
 botao.addEventListener('click', abreMenu);
+botao.addEventListener('touchstart', abreMenu);
