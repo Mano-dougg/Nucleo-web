95363c2ef94279493b4ccc48404443c6009ad69d
Bruno-Emanoel <brunoemanoel.l.x@gmail.com>
2024-05-09 20:50:24 -0300

    feat: creating the hamburguer tab and making the code for making it toggle with the button click

---
+++ 2-PlantCard/script.js
@@ -0,0 +1,7 @@
+//getting the reference for the hamburguer tab
+const optionList = document.getElementById("post-options__list")
+
+//this function toggles the visibility of the tab using the "invisible" class
+const toggleOptions = ()=>{
+    optionList.classList.toggle("invisible")
+}
