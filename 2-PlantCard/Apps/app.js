1187dc31474b39b3d164266e72b479227a0401a8
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-09 19:43:36 -0300

    fix: Melhorada legibilidade do javascript

--- 2-PlantCard/Apps/app.js
+++ 2-PlantCard/Apps/app.js
@@ -1,13 +1,21 @@
 const botao = document.getElementById('botaoShare');
 
 function abreMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault();
+
+    if (event.type === 'touchstart')
+        event.preventDefault();
+
     const share = document.getElementById('compartilhar');
     share.classList.toggle('ativo');
+
     const ativou = share.classList.contains('ativo');
     event.currentTarget.setAttribute('aria-expanded', ativou);
-    if(ativou) { event.currentTarget.setAttribute('aria-label', '"fecha opções de compartilhamento"');
-} else { event.currentTarget.setAttribute('aria-label', '"abre opções de compartilhamento"') }
+    
+    if(ativou) {
+        event.currentTarget.setAttribute('aria-label', '"fecha opções de compartilhamento"');
+    } else {
+        event.currentTarget.setAttribute('aria-label', '"abre opções de compartilhamento"')
+    }
 }
 
 botao.addEventListener('click', abreMenu);
