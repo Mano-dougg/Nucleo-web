2bdb73c180572f281c5956f860a384e563308353
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-08 23:15:49 -0300

    fix: Implementada função de aparecer e sumir aba de compartilhar

--- 2-PlantCard/Apps/app.js
+++ 2-PlantCard/Apps/app.js
@@ -1,2 +1,8 @@
 const botao = document.getElementById('botaoShare');
+
+function abreMenu() {
+    const share = document.getElementById('compartilhar');
+    share.classList.toggle('ativo');
+}
+
 botao.addEventListener('click', abreMenu);
