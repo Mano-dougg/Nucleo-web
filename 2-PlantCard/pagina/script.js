91073913c07bf73078f482f76fc15c66aabac11b
Matheus Dias <matheuscasmd@gmail.com>
2024-05-10 17:41:00 -0300

    finalizacao de abrir o menu hamburguer nas duas páginas, falta ajustar na primeira que deu choque no js

--- 2-PlantCard/pagina/script.js
+++ 2-PlantCard/pagina/script.js
@@ -1,4 +1,4 @@
-function expandirCompartilhamento(){
+function expandirCompartilhamentoDesktop(){
     var div1 = document.getElementById('compartilhamento');
     var div2 = document.getElementById('autor');
     if(div1.style.display == 'none'){
@@ -7,5 +7,25 @@ function expandirCompartilhamento(){
     } else {
         div2.style.marginTop = '133px';
         div1.style.display = 'none';
+    }
+}
+function expandirCompartilhamentoMobile(){
+    var div1 = document.getElementById('compartilhamento');
+    var div2 = document.getElementById('autor');
+    if(div1.style.display == 'none'){
+        div1.style.display = 'block';
+        div2.style.marginTop = '0px';
+    } else {
+        div2.style.marginTop = '88px';
+        div1.style.display = 'none';
+    }
+}
+function expandirCompartilhamento(){
+    var div3 = document.getElementById('card');
+    var check = div3.style.maxWidth <= 450;
+    if(check){
+        expandirCompartilhamentoMobile();
+    } else {
+        expandirCompartilhamentoDesktop();
     }
 }
