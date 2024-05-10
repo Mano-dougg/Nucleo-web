99b8a0519cbe46a850fef8b6c5e66f45b931988a
Cristiano Santos Ribeiro Filho <cristiano.sr.filho@proton.me>
2024-05-09 22:25:53 -0300

    Mudanças feitas: Adicionado Javascript com DOM API - Projeto 2 Funcional!

---
+++ 2-PlantCard/main.js
@@ -0,0 +1,16 @@
+// Definição das variaveis 
+var menu = document.querySelector("button.sharing-button")
+var botão = document.querySelector("button.press-active")
+// Definir Padrão do menu e EventListener
+menu.style.display = "none"
+botão.addEventListener("click")
+// Definir Evento de Clickar 
+function openOrclosemenu(){
+    if (menu.style.display == "none") {
+        menu.style.display = "flex"
+    }
+    else {
+        menu.style.display = "none"
+    }
+}
+
