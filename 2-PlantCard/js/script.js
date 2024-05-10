70cc862492317f3f398ffa824ad2f58097fdec92
Bruno-Emanoel <brunoemanoel.l.x@gmail.com>
2024-05-09 21:21:41 -0300

    style: organizing the files of the project(i realized it would be better to do it before just now) and renaming the classes due to the suggestions in the last merge request

--- 2-PlantCard/js/script.js
+++ 2-PlantCard/js/script.js
@@ -1,5 +1,5 @@
 //getting the reference for the hamburguer tab
-const optionList = document.getElementById("post-options__list")
+const optionList = document.getElementById("content-options-list")
 
 //this function toggles the visibility of the tab using the "invisible" class
 const toggleOptions = ()=>{
