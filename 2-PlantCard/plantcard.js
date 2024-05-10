f5b911ef9dd2b8f1f8795dd2c3cd9e7ce7e614c4
Mauricio Matchal <mauriciomatchal@gmail.com>
2024-05-10 00:48:33 -0300

    adição de animações no modal de redes sociais

--- 2-PlantCard/plantcard.js
+++ 2-PlantCard/plantcard.js
@@ -1,10 +1,13 @@
 let popupDrawer = document.getElementById("popup-drawer");
+let shareButton = document.getElementById("share-btn");
 
 
 function sharePopUp() {
   if (popupDrawer.style.visibility == "hidden") {
     popupDrawer.style.visibility = "visible";
+    shareButton.style.backgroundColor = "rgb(55, 58, 45)";
   } else {
     popupDrawer.style.visibility = "hidden";
+    shareButton.style.backgroundColor = "rgba(105, 111, 90, 1)"
   }
 };
