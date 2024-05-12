e57ef6d7288f1c082cba3d4da39ff9f8ae1d0783
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-05-12 12:28:10 -0300

    feat: criando a estrutura/esqueleto da Section1

--- 3-Portifolio/src/App.jsx
+++ 3-Portifolio/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header.jsx'
+import Section1 from './components/Section1/Section1.jsx'
 function App() {
 
   return (
     <>
      <Header/>
+     <Section1/>
     </>
   )
 }
---
+++ 3-Portifolio/src/components/Section1/Section1.jsx
@@ -0,0 +1,41 @@
+
+
+function Section1() {
+    return (
+        <>
+        <section>
+            <article>
+                <img src="" alt="" />
+                <h1>Hello. 
+                <br/>My name is Douglas
+                </h1>
+                <hr />
+            </article>
+            <article>
+                <p>
+                        I'm a Digital Product and UI Designer – creating digital experiences that are intuitive 
+                    for real people and making complex processes easy to use.
+                <br />
+                <br />
+                        Right now, I'm Design Lead at <li><a href="">Holiday Extras</a></li>, covering all our digital platforms across
+                    a whole load of brands – leading a great team across <li><a href=""> design</a></li>, UI engineering, accessibility
+                    and customer experience. I'm lucky to be part of a great team of designers, writers,
+                    engineers, PMs and data specialists – amongst others – testing, iterating and optimising our platforms and products.
+                <br />
+                <br />
+                        I've got some <li><a href=""> work on Dribbble </a></li>, some previous work at <li><a href=""> Saga </a></li> 
+                    and you can find me over on twitter and sometimes <li><a href=""> on Medium too </a></li>. I also take
+                    <li><a href=""> too many photos </a></li>.
+                </p>
+                    <div className={Styles.callOut}>
+                        <img src="" alt="" />
+                        <p>Available for work and general design goodness – <li><a href=""> say hello </a></li>
+                        </p>
+                    </div>
+            </article>
+        </section>
+        </>
+    )
+}
+
+export default Section1
