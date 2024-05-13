03394d967f592c367f7928402de5aac14a671494
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-05-13 15:26:41 -0300

    feat: estilizando a Section2

--- 3-Portifolio/src/components/Section2/Section2.jsx
+++ 3-Portifolio/src/components/Section2/Section2.jsx
@@ -1,16 +1,16 @@
+import styles from './Section2.module.css'
 
 function Section2(){
     return(
         <>
-        <section>
+        <section className={styles.Section2}>
             <hr />       
-            <div>
+            <div className={styles.divTitle}>
                 <h2>Title</h2>
             </div>
             <p>
-                    Line of copy in here to decribe this section. Line of copy in here to decribe
-                    <br />this section. Line of copy in here to decribe this section. Line of copy 
-                    in here to decribe this section. 
+                    Line of copy in here to decribe this section. Line of copy in here to decribe this section.Line of copy in here to decribe
+                <br /> this section. Line of copy in here to decribe this section. 
             </p>
         </section>
         </>
