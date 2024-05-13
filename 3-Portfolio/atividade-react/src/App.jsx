df75121f10cc9d2fe454cc563e55cf93d64e25e6
EderNatan.dev <edernatan124@gmail.com>
2024-05-13 11:53:38 -0300

    theme dark

--- 3-Portfolio/atividade-react/src/App.jsx
+++ 3-Portfolio/atividade-react/src/App.jsx
@@ -1,20 +1,34 @@
-import './App.css'
-import Header from './components/Header'
-import Section1 from './components/Section1'
-import Section2 from './components/Section2'
+
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (darkMode) {
+      document.body.classList.remove('dark-mode');
+    } else {
+      document.body.classList.add('dark-mode');
+    }
+  };
 
   return (
-    <>
+    <div className={darkMode ? 'dark-mode' : ''}>
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <main>
-        <Header />
         <div className='sections'>
-          <Section1/>
-          <Section2/>
+          <Section1 />
+          <Section2 />
         </div>
       </main>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- 3-Portfolio/atividade-react/src/components/Header/index.jsx
+++ 3-Portfolio/atividade-react/src/components/Header/index.jsx
@@ -1,52 +1,72 @@
-import styles from './Header.module.css'
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import styles from './Header.module.css';
 
-function Header(){
-    return(
-        <header className={styles.header}>
-            <nav className={styles.navbar}>
-            <div className={styles.itens_nav1}>
-                <a className={styles.logo} href="#">Eder Natan</a>
-                <a href="#">Section Segund</a>
-                <a href="#">Section three</a>
-                <a href="#">Section four</a>
+function Header() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (darkMode) {
+      document.body.classList.remove('dark-mode');
+    } else {
+      document.body.classList.add('dark-mode');
+    }
+  };
+
+  return (
+    <header className={darkMode ? `${styles.header} ${styles.dark}` : styles.header}>
+      <nav className={styles.navbar}>
+        <div className={styles.itens_nav1}>
+          <a className={styles.logo} href="#">
+            Eder Natan
+          </a>
+          <a href="#">Section Segund</a>
+          <a href="#">Section three</a>
+          <a href="#">Section four</a>
+        </div>
+        <div>
+          <ul className={styles.itens_nav2}>
+            <div className={styles.itens_img}>
+              <li>
+                <a href="#">
+                  <img src="/src/assets/img/twitter-icon.svg" alt="Twitter" />
+                </a>
+              </li>
+
+              <li>
+                <a href="#">
+                  <img src="/src/assets/img/Vector(1).svg" alt="" />
+                </a>
+              </li>
+
+              <li>
+                <a href="#">
+                  <img src="/src/assets/img/instagram-icon.svg" alt="" />
+                </a>
+              </li>
             </div>
-            <div >
-                <ul className={styles.itens_nav2} >
-                    <div className={styles.itens_img} >
-                        <li><a href="#"><img src="/src/assets/img/twitter-icon.svg" alt="Twitter" /></a>
-                            
-                        </li>
-                        
-                        <li><a href="#"><img src="/src/assets/img/Vector(1).svg" alt="" /></a>
-                        </li>
-                        
-                        <li><a href="#"><img src="/src/assets/img/instagram-icon.svg" alt="" /></a>
-                    
-                        </li>
-                    </div>
 
-                    <div className={styles.itens_dark}>
-                        <li>Dark Mode : </li>
-                            <div className={styles.itens_checkbox}>
-                                <input type="checkbox" className={styles.checkbox} id='chk'/>
-                                <label className={styles.label} >
-                               
-                                    <div className={styles.ball}></div>
-                                     </label>
-                            </div>
-                    </div>
-                    
-                    
-                    
-                </ul>
+            <div className={styles.itens_dark}>
+              <li>Dark Mode :</li>
+              <div className={styles.itens_checkbox}>
+                <input
+                  type="checkbox"
+                  className={styles.checkbox}
+                  id="chk"
+                  checked={darkMode}
+                  onChange={toggleDarkMode}
+                />
+                <label className={styles.label} htmlFor="chk">
+                  <div className={styles.ball}></div>
+                </label>
+              </div>
             </div>
-            
-
-        </nav>
-
-        </header>
-
-    )
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
