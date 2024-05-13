e5b913b635d0262e1526c09864ae086e90eb086f
EderNatan.dev <edernatan124@gmail.com>
2024-05-12 23:10:29 -0300

    two section

--- 3-Portfolio/atividade-react/src/App.jsx
+++ 3-Portfolio/atividade-react/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Section1 from './components/Section1'
+import Section2 from './components/Section2'
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
         <Header />
         <div className='sections'>
           <Section1/>
+          <Section2/>
         </div>
       </main>
     </>
---
+++ 3-Portfolio/atividade-react/src/components/Section2/index.jsx
@@ -0,0 +1,53 @@
+import './Section2.css'
+
+const Section2 = () => {
+    return (
+        <>
+       <section className="section2">
+            <div className="hr2">
+                <div className="espaco"></div>
+    <div className="conteudo2">
+      <h2>Title</h2>
+      <p>Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. </p>
+    </div>
+    
+    <div className="conteudo2-2">
+        <div className="conteudo2-colum1">
+            <img src="/src/assets/img/img2.jpeg" style={{ width: '100%' }} alt="" />
+        </div>
+        <div className="conteudo2-colum2">
+            <div className="text">
+                <h3>Title</h3>
+                <p>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
+                <button>Button primary light</button>
+            </div>
+        </div>
+    
+    </div>
+
+    <div className="conteudo2-3">
+        <div className="conteudo2-colum1">
+        <img src="/src/assets/img/img2.jpeg" style={{ width: '100%' }} alt="" />
+
+
+        </div>
+        <div className="conteudo2-colum2">
+            <div className="text2">
+                <h3>Titless</h3>
+                <p>Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.</p>
+                <button>Button primary light</button>
+            </div>
+        </div>
+    
+    </div>
+    </div>
+
+
+            
+
+        </section>
+        </>
+    )
+}
+
+export default Section2
