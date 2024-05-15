31bbe10fc9e3df8a7aa8a11da6b50d0d9b97d0c4
Carlos Eduardo <cast9zzmast@gmail.com>
2024-05-15 08:49:37 -0300

    init: entendendo o que e React

---
+++ 3-Portfolio/meu-portfolio/Desenvolvimento-central/primeirosPassos.js
@@ -0,0 +1,11 @@
+function primeirosPassos() {
+
+    return (
+        <div>
+            <h1>
+                Olá, primeiros passos em React!
+            </h1>
+        </div>
+    )
+}
+export default primeirosPassos
