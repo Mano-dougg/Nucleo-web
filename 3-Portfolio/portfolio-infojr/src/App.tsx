957ca80cbb2bec7f6df84ccb90d624266e530302
Fernando Costa <costa.fernando@ufba.br>
2024-05-15 14:29:13 -0300

    feat: implements theme context API

--- 3-Portfolio/portfolio-infojr/src/App.tsx
+++ 3-Portfolio/portfolio-infojr/src/App.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import ThemeContext from "./context/ThemeContext";
+
 export default function App() {
+  const [ isDarkThemed, toggleDarkTheme ] = useState<boolean>(false)
+  function toggleTheme() {
+    toggleDarkTheme(!isDarkThemed)
+  }
   return (
-    <>
-    </>
+    <ThemeContext.Provider value={{ isDarkThemed, toggleTheme }}>
+    </ThemeContext.Provider>
   )
 }
---
+++ 3-Portfolio/portfolio-infojr/src/context/ThemeContext.ts
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+type ThemeContextType = {
+  isDarkThemed: boolean;
+  toggleTheme: () => void;
+};
+
+const ThemeContext = createContext({} as ThemeContextType);
+
+
+export default ThemeContext;
