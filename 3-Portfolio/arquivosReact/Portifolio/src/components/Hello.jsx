abdd23f322a5f279c4e2031cd0011dd11ecf9d3b
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-16 11:41:02 -0300

    fix: Implementado parte do CSS inicial

--- 3-Portfolio/arquivosReact/Portifolio/src/components/Hello.jsx
+++ 3-Portfolio/arquivosReact/Portifolio/src/components/Hello.jsx
@@ -10,14 +10,14 @@ function Hello(props) {
                 I'm a Digital Product and UI Designer – creating digital experiences that
                 are intuitive for real people and making complex processes easy to use.
                 <br /><br />
-                Right now, I'm Design Lead at Holiday Extras, covering all our digital
-                platforms across a whole load of brands – leading a great team across design,
+                Right now, I'm Design Lead at <a>Holiday Extras</a>, covering all our digital
+                platforms across a whole load of brands – leading a great team across <a>design</a>,
                 UI engineering, accessibility and customer experience. I'm lucky to be part
                 of a great team of designers, writers, engineers, PMs and data specialists –
                 amongst others – testing, iterating and optimising our platforms and products.
                 <br /><br />
-                I've got some work on Dribbble, some previous work at Saga and you can find me
-                over on twitter and sometimes on Medium too. I also take too many photos.
+                I've got some <a>work on Dribbble</a>, some previous work at <a>Saga</a> and you can find me
+                over on twitter and sometimes <a>on Medium too</a>. I also take <a>too many photos</a>.
             </p>
             <div>
                 <img src={Check} />
