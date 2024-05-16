c5458d082b891e603fc4d301e5394165b3c923ef
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-16 14:27:48 -0300

    fix: trocado html puro por chamada ao Galeria

--- 3-Portfolio/arquivosReact/Portifolio/src/App.jsx
+++ 3-Portfolio/arquivosReact/Portifolio/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Hello from './components/Hello';
+import Galeria from './components/Galeria';
 
 
 function App(props) {
@@ -11,33 +12,7 @@ function App(props) {
       <Header nome={nome} sobrenome={sobrenome} />
       <main>
         <Hello nome={nome} />
-        <h2>Title</h2>
-        <p>
-          Line of copy in here to decribe this section. Line of copy in here to decribe this section.
-          Line of copy in here to decribe this section. Line of copy in here to decribe this section.
-        </p>
-        <div>
-          {/*<img src'Imagem aqui' />*/}
-          <div>
-            <h3>Title</h3>
-            <p>
-            Leading, implementing and evolving engaging customer experiences
-            and UI foundations for every touch-point across various platforms.
-            </p>
-            <button>Button Primary Light</button>
-          </div>
-        </div>
-        <div>
-          {/*<img src'Imagem aqui' />*/}
-          <div>
-            <h3>Title</h3>
-            <p>
-            Leading, implementing and evolving engaging customer experiences
-            and UI foundations for every touch-point across various platforms.
-            </p>
-            <button>Button Primary Light</button>
-          </div>
-        </div>
+        <Galeria />
       </main>
     </>
   )
