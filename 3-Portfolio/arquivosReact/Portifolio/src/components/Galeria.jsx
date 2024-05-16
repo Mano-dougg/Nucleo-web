4db198758889daa2af8fa9648231db80276f1b6d
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-16 14:55:36 -0300

    fix: Corrigidas proporções das imagens

--- 3-Portfolio/arquivosReact/Portifolio/src/components/Galeria.jsx
+++ 3-Portfolio/arquivosReact/Portifolio/src/components/Galeria.jsx
@@ -1,3 +1,7 @@
+import Foto1 from '../assets/imagem1.jpg'
+import Foto2 from '../assets/imagem2.png'
+
+
 function Galeria() {
     return (
         <div className="galeria">
@@ -7,26 +11,26 @@ function Galeria() {
             Line of copy in here to decribe this section. Line of copy in here to decribe this section.
             </p>
             <div className="galeriaItem">
-            {/*<img src'Imagem aqui' />*/}
-            <div>
-                <h3>Title</h3>
-                <p>
-                Leading, implementing and evolving engaging customer experiences
-                and UI foundations for every touch-point across various platforms.
-                </p>
-                <button>Button Primary Light</button>
-            </div>
+            <div><img src={Foto1} /></div>
+                <div>
+                    <h3>Title</h3>
+                    <p>
+                    Leading, implementing and evolving engaging customer experiences
+                    and UI foundations for every touch-point across various platforms.
+                    </p>
+                    <button>Button Primary Light</button>
+                </div>
             </div>
             <div className="galeriaItem" style={{flexDirection: "row-reverse"}}>
-            {/*<img src'Imagem aqui' />*/}
-            <div>
-                <h3>Title</h3>
-                <p>
-                Leading, implementing and evolving engaging customer experiences
-                and UI foundations for every touch-point across various platforms.
-                </p>
-                <button>Button Primary Light</button>
-            </div>
+                <div><img src={Foto2} /></div>
+                <div>
+                    <h3>Title</h3>
+                    <p>
+                    Leading, implementing and evolving engaging customer experiences
+                    and UI foundations for every touch-point across various platforms.
+                    </p>
+                    <button>Button Primary Light</button>
+                </div>
             </div>
         </div>
     );
