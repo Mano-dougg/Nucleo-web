b52125dbc45d6b9faa9a608ac8854c0483b6ea07
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-05-16 10:09:47 -0300

    criando a estrutura do menu mobile

--- 3-Portifolio/src/components/Header/Header.jsx
+++ 3-Portifolio/src/components/Header/Header.jsx
@@ -7,9 +7,10 @@ function Header(){
     return(
     <>
     <header>
+        {/* MENU DESKTOP */}
         <article className={styles.articleHeader}>
             <h4 className={styles.h4SemPaddingBottom}> <a className={styles.linkTitulo} href="#Section1">Douglas Aleixo</a></h4>
-            <nav>
+            <nav className={styles.navDesktop}>
                 <ul>
                     <li><a href="#Section2">Section two</a></li>
                     <li><a href="#Section3">Section three</a></li>
@@ -18,7 +19,7 @@ function Header(){
             </nav>
         </article>
         <article className={styles.articleHeader}>
-            <div className={styles.iconsHeader}>
+            <div className={styles.iconsHeaderDesktop}>
                 <img className={styles.IconTwitter} src={twitterIcon} alt="Logo do twitter" />   
                 <img className={styles.IconFigma} src={figmaIcon} alt="Logo do figma" />
                 <img className={styles.IconInsta} src={InstaIcon} alt="Logo do Instagram" />
@@ -29,6 +30,31 @@ function Header(){
                 <label htmlFor="check" className={styles.switch}>
                     <span className={styles.slider}></span>
                 </label>
+            </div>
+        </article>
+
+        {/* MENU MOBILE */}
+        <article className={styles.menuMobile}>
+            <div>
+                <button id='styles.btn-menu'>
+                    <span className={styles.linha}></span>
+                    <span className={styles.linha}></span>
+                    <span className={styles.linha}></span>
+                </button>
+            </div>
+            <div>
+                <nav className={styles.navMobile}>
+                    <ul>
+                        <li><a href="#Section2">Section two</a></li>
+                        <li><a href="#Section3">Section three</a></li>
+                        <li><a href="#Section4">Section four</a></li>
+                    </ul>
+                </nav>
+                <div className={styles.iconsHeaderMobile}>
+                    <img className={styles.IconTwitter} src={twitterIcon} alt="Logo do twitter" />   
+                    <img className={styles.IconFigma} src={figmaIcon} alt="Logo do figma" />
+                    <img className={styles.IconInsta} src={InstaIcon} alt="Logo do Instagram" />
+                </div>
             </div>
         </article>
     </header>
