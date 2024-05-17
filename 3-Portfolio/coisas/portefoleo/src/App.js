0eadf28ecbb9ee33e2a22d1874d5c71d0b22c605
talitaester <talita.alves@ufba.br>
2024-05-16 21:37:25 -0300

    darkmode

--- 3-Portfolio/coisas/portefoleo/src/App.js
+++ 3-Portfolio/coisas/portefoleo/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { AiOutlineTikTok, AiOutlineTwitter } from "react-icons/ai";
-import { AiOutlineInstagram } from "react-icons/ai";
-import { AiFillTikTok } from "react-icons/ai";
+import { AiOutlineTikTok, AiOutlineTwitter, AiOutlineInstagram } from "react-icons/ai";
+import { BsCheckCircleFill, BsXCircle, BsList } from "react-icons/bs";
 import './app.css';
 import cutao from './assets/cutao.png';
 import culita from './assets/culita.png';
@@ -14,6 +13,7 @@ import ufba from './assets/cufba.png';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   const clickMenu = (event) => {
     event.preventDefault();
@@ -26,58 +26,52 @@ function App() {
   };
 
   const toggleDarkMode = () => {
-    const currentColor = getComputedStyle(document.documentElement).getPropertyValue('--corprincipal').trim();
-    if (currentColor === '#7400b2') {
-      document.documentElement.style.setProperty('--corprincipal', '#ffffff');
-    } else {
-      document.documentElement.style.setProperty('--corprincipal', '#7400b2');
-    }
+    setDarkMode(!darkMode);
+    document.body.classList.toggle('dark-mode', !darkMode);
   };
 
-
   return (
-    <body>
-    <div className='body'>
+    <div className={`body ${darkMode ? 'dark-mode' : ''}`}>
       <div className="header">
         <div className="headerleft">
           <div className="homename">
             <a href=".header">Talita Ester</a>
           </div>
           <div className="sections">
-            <a href="">section two</a>
-            <a href="">section three</a>
-            <a href="">section four</a>
+            <a href="#">section two</a>
+            <a href="#">section three</a>
+            <a href="#">section four</a>
           </div>
         </div>
         <div className="headerright">
           <div className="redes">
-            <a href=""><AiOutlineTwitter size={22} color="#7400b2" /></a>
-            <a href=""><AiOutlineInstagram size={22} color="#7400b2" /></a>
-            <a href=""><AiOutlineTikTok size={22} color="#7400b2" /></a>
+            <a href="#"><AiOutlineTwitter size={22} color="var(--corprincipal)" /></a>
+            <a href="#"><AiOutlineInstagram size={22} color="var(--corprincipal)" /></a>
+            <a href="#"><AiOutlineTikTok size={22} color="var(--corprincipal)" /></a>
           </div>
           <div className="darkmode">
-            <p onClick={toggleDarkMode()} >Dark mode:</p>
-            <a href=""><img src={cutao} alt="cutao" className="cutao" width="40" height="22" /></a>
+            <p onClick={toggleDarkMode}>Dark mode:</p>
+            <a onClick={toggleDarkMode} href="#"><img src={cutao} alt="cutao" className="cutao" width="40" height="22" /></a>
           </div>
         </div>
       </div>
       <div className="main">
         {!menuOpen && (
-          <a onClick={clickMenu} className="cunu" id="cunu" href="#"><img src={cunu} className="botaox" width="30" height="30" alt="menu" /></a>
+          <a onClick={clickMenu} className="cunu" id="cunu" href="#"><BsList size={30} color="var(--corprincipal)" className="botaox" alt="menu" /></a>
         )}
         {menuOpen && (
           <div className="hamdiv" id="hamdiv">
             <div className="hamburger">
-              <a onClick={clickXis} href="#" className="xis"><img src={xis} className="botaox" width="30" height="30" alt="close" /></a>
+              <a onClick={clickXis} href="#" className="xis"><BsXCircle className="botaox" size={30} color="var(--corprincipal)" alt="close" /></a>
               <div className="sectionsham">
                 <a href="#">section two</a>
                 <a href="#">section three</a>
                 <a href="#">section four</a>
               </div>
               <div className="redesham">
-                <a href="#"><AiOutlineTwitter size={22} color="#7400b2" /></a>
-                <a href="#"><AiOutlineInstagram size={22} color="#7400b2" /></a>
-                <a href="#"><AiOutlineTikTok size={22} color="#7400b2" /></a>
+                <a href="#"><AiOutlineTwitter size={22} color="var(--corprincipal)" /></a>
+                <a href="#"><AiOutlineInstagram size={22} color="var(--corprincipal)" /></a>
+                <a href="#"><AiOutlineTikTok size={22} color="var(--corprincipal)" /></a>
               </div>
             </div>
           </div>
@@ -93,33 +87,27 @@ function App() {
             <p className="texto">I've got some work on Dribbble, some previous work at Saga and you can find me over on twitter and sometimes on Medium too. I also take too many photos.</p>
           </div>
           <div className="work">
-            <img src={cuk} className="cula" width="16" height="16" alt="Available for work" />
-            <p className="ava">Available for work and general design goodness <span style={{ color: '#7400b2' }}>- say hello</span></p>
+            <BsCheckCircleFill size={16} color="var(--corprincipal)"/>
+            <p className="ava"> <span style={{ color: 'var(--text-color)' }}>Available for work and general design goodness</span> <span style={{ color: 'var(--corprincipal)' }}>- say hello</span></p>
           </div>
         </div>
         <div className='linha2'></div>
         <div className='posts'>
-          
-          <h1 style={{paddingBottom: '5px'}}>Remarkables</h1>
-          <p style={{fontSize: '17px'} }> Section to tell you about me and the things i've done in the name of computer science!</p>
+          <h1 style={{ paddingBottom: '5px' }}>Remarkables</h1>
+          <p style={{ fontSize: '17px' }}> Section to tell you about me and the things i've done in the name of computer science!</p>
           <Post
-          image={ufba}
-          title="Getting into UFBA"
-          content="I have loved technology since i was little, because of my father and my brother, but my real jouney started in 2022 when i decided to graduate in CS.
-                  The University Federal of Bahia was my first option ever and i was afraid i couldn't get into it, but everything happend he right way
-                  today i'm two years into my graduation and never been so happy with my choices"
+            image={ufba}
+            title="Getting into UFBA"
+            content="I have loved technology since i was little, because of my father and my brother, but my real jouney started in 2022 when i decided to graduate in CS. The University Federal of Bahia was my first option ever and i was afraid i couldn't get into it, but everything happend he right way today i'm two years into my graduation and never been so happy with my choices"
           />
           <Post
-          image={webmedia}
-          title="scientific research"
-          content="In my first semester at UFBA, i got into a research project where i learned an infinity of things about programming and science in general.
-          My project was about a mobility model for the objects of the internet of things, based on a already existent model: the SWIM.
-          The model was well accepted and the results of the research was publicated by the WebMedia2023"
+            image={webmedia}
+            title="scientific research"
+            content="In my first semester at UFBA, i got into a research project where i learned an infinity of things about programming and science in general. My project was about a mobility model for the objects of the internet of things, based on a already existent model: the SWIM. The model was well accepted and the results of the research was publicated by the WebMedia2023"
           />
-          </div>
+        </div>
       </div>
     </div>
-    </body>
   );
 }
 
