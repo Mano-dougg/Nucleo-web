498c3987b1ca9ad1746df66f0eeabc74fb79efbd
lucasdsmachado <briandsmachado@gmail.com>
2024-05-18 20:22:08 -0300

    chore: finish challenge

--- 3-Portfolio/src/App.jsx
+++ 3-Portfolio/src/App.jsx
@@ -1,14 +1,44 @@
 import { Header } from './components/Header.jsx'
 import { Home } from './components/Home.jsx';
+import { SideMenu } from './components/SideMenu.jsx';
+import { useEffect, useState } from 'react';
+import { ToggleMenuButton } from './components/ToggleMenuButton.jsx';
 import './App.module.css'
 import './global.css';
 
 function App() {
 
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const updateWindowWidth = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', updateWindowWidth);
+
+    return () => {
+      window.removeEventListener('resize', updateWindowWidth);
+    };
+  }, []);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    if (windowWidth > 870 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [menuOpen, windowWidth]);
+
   return (
     <>
       <Header />
-      <Home />
+      <ToggleMenuButton menuOpen={menuOpen} handleToggleMenu={handleToggleMenu} />
+      <SideMenu menuOpen={menuOpen} />
+      {!menuOpen && <Home />}
     </>
   )
 }
--- 3-Portfolio/src/components/Header.jsx
+++ 3-Portfolio/src/components/Header.jsx
@@ -1,28 +1,26 @@
 import { TwitterFill, FigmaFill, InstagramFill } from 'akar-icons'
 import styles from './Header.module.css';
-import { useState } from 'react';
 
 function toggleDarkTheme() {
   document.body.classList.add('dark');
 }
 
 export function Header() {
-  
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_left}>
         <span className={styles.brand}>Brian Lucas</span>
         <ul className={styles.nav_links}>
-          <li><a href="#" className={styles.nav_link}>Link 1</a></li>
-          <li><a href="#" className={styles.nav_link}>Link 2</a></li>
-          <li><a href="#" className={styles.nav_link}>Link 3</a></li>
+          <li><a href="#" className={styles.nav_link}>Section two</a></li>
+          <li><a href="#" className={styles.nav_link}>Section three</a></li>
+          <li><a href="#" className={styles.nav_link}>Section four</a></li>
         </ul>
       </div>
       <div className={styles.navbar_right}>
         <div className={styles.social_links}>
-          <a href="#" className={styles.social_link}><TwitterFill /></a>
-          <a href="#" className={styles.social_link}><FigmaFill /></a>
-          <a href="#" className={styles.social_link}><InstagramFill /></a>
+          <a href="#" className={styles.social_link}><TwitterFill size={20} /></a>
+          <a href="#" className={styles.social_link}><FigmaFill size={20}  /></a>
+          <a href="#" className={styles.social_link}><InstagramFill size={20}  /></a>
         </div>
         <span className={styles.theme_toggle}>Dark mode: <button id={styles.togglebutton} onClick={toggleDarkTheme}>Toggle</button></span>
       </div>
