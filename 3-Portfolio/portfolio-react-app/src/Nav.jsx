1a53dc46807b0528474d3e7c7c205a451ac31bd8
Matheus Dias <matheuscasmd@gmail.com>
2024-05-19 18:57:03 -0300

    finalização nav

--- 3-Portfolio/portfolio-react-app/src/Nav.jsx
+++ 3-Portfolio/portfolio-react-app/src/Nav.jsx
@@ -1,8 +1,28 @@
-import reactLogo from './assets/twitter.svg'
+import twitterLogo from './assets/twitter.svg'
+import figmaLogo from './assets/figma.svg'
+import instagramLogo from './assets/instagram.svg'
 function Nav() {
   return (
     <>
-      <img src={reactLogo} alt="react" />
+      <nav id="nav">
+                <ul className="nav-items">
+                    <li className="list-item"><h4>Kevin Bennet</h4></li>
+                    <li className="list-item"><a href="section-two"><span className="azul">Section two</span></a></li>
+                    <li className="list-item"><a href="section-three"><span className="azul">Section three</span></a></li>
+                    <li className="list-item"><a href="section-two"><span className="azul">Section four</span></a></li>
+                    <li className="list-icon"><a href="#"><img src={twitterLogo} alt="twitter" /></a></li>
+                    <li className="list-icon"><a href="#"><img src={figmaLogo} alt="figma" /></a></li>
+                    <li className="list-icon"><a href="#"><img src={instagramLogo} alt="" /></a></li>
+                    <li className="theme-button">
+                        <div className="dark-mode">
+                            <label className="switch">Dark mode:
+                                <input type="checkbox" />
+                                <span className="slider round"></span>
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
     </>
   )
 }
