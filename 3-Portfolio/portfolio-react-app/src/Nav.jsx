9cc8b2d594a652448eec2d0387e7426b1a0f1cde
Matheus Dias <matheuscasmd@gmail.com>
2024-05-19 22:20:33 -0300

    base do css nav

--- 3-Portfolio/portfolio-react-app/src/Nav.jsx
+++ 3-Portfolio/portfolio-react-app/src/Nav.jsx
@@ -1,18 +1,21 @@
 import twitterLogo from './assets/twitter.svg'
 import figmaLogo from './assets/figma.svg'
 import instagramLogo from './assets/instagram.svg'
+import './styles/nav.css'
 const Nav = () => {
   return (
     <>
-      <nav id="nav">
+      <nav className='nav'>
                 <ul className="nav-items">
-                    <li className="list-item"><h4>Kevin Bennet</h4></li>
+                    <li className="list-header"><h4>Kevin Bennet</h4></li>
                     <li className="list-item"><a href="section-two"><span className="azul">Section two</span></a></li>
                     <li className="list-item"><a href="section-three"><span className="azul">Section three</span></a></li>
                     <li className="list-item"><a href="section-two"><span className="azul">Section four</span></a></li>
-                    <li className="list-icon"><a href="#"><img src={twitterLogo} alt="twitter" /></a></li>
-                    <li className="list-icon"><a href="#"><img src={figmaLogo} alt="figma" /></a></li>
-                    <li className="list-icon"><a href="#"><img src={instagramLogo} alt="" /></a></li>
+                    <ul className='nav-icons'>
+                        <li className="list-icon"><a href="#"><img src={twitterLogo} alt="twitter" /></a></li>
+                        <li className="list-icon"><a href="#"><img src={figmaLogo} alt="figma" /></a></li>
+                        <li className="list-icon"><a href="#"><img src={instagramLogo} alt="" /></a></li>
+                    </ul>
                     <li className="theme-button">
                         <div className="dark-mode">
                             <label className="switch">Dark mode:
@@ -22,7 +25,7 @@ const Nav = () => {
                         </div>
                     </li>
                 </ul>
-            </nav>
+        </nav>
     </>
   )
 }
