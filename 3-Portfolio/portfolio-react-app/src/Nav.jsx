5a8d33c5d1b7db1dbc87b689deb244eeefbb6869
Matheus Dias <matheuscasmd@gmail.com>
2024-05-20 09:33:06 -0300

    troca de figma por linkedin

--- 3-Portfolio/portfolio-react-app/src/Nav.jsx
+++ 3-Portfolio/portfolio-react-app/src/Nav.jsx
@@ -1,5 +1,5 @@
 import twitterLogo from './assets/twitter.svg'
-import figmaLogo from './assets/figma.svg'
+import linkedinLogo from './assets/linkedin.svg'
 import instagramLogo from './assets/instagram.svg'
 import './styles/nav.css'
 const Nav = () => {
@@ -13,7 +13,7 @@ const Nav = () => {
                     <li className="list-item"><a href="section-two"><span className="azul">Section four</span></a></li>
                     <ul className='nav-icons'>
                         <li className="list-icon"><a href="#"><img src={twitterLogo} alt="twitter" /></a></li>
-                        <li className="list-icon"><a href="#"><img src={figmaLogo} alt="figma" /></a></li>
+                        <li className="list-icon"><a href="#"><img src={linkedinLogo} alt="linkedin" /></a></li>
                         <li className="list-icon"><a href="#"><img src={instagramLogo} alt="" /></a></li>
                     </ul>
                     <li className="theme-button">
