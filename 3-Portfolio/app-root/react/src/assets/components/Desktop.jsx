1dcd434f7447819d83c1f8dabe8c639a0d90fc38
Cristiano Santos Ribeiro Filho <cristiano.sr.filho@proton.me>
2024-05-19 21:26:36 -0300

    Falhas corrigidas no Projeto, e funcionalidades faltando adicionadas - Projeto 3 Finalizado - Aguardando Retorno

--- 3-Portfolio/app-root/react/src/assets/components/Desktop.jsx
+++ 3-Portfolio/app-root/react/src/assets/components/Desktop.jsx
@@ -3,19 +3,22 @@ import IconSection from "./nav-items/IconSection"
 import ToggleSwitch from "./nav-items/ToggleSwitch"
 import Section1 from "./content-items/Section1"
 import Section2 from "./content-items/Section2"
+import LinkSectionC from "./content-items/LinkSectionC"
+import IconSectionC from "./content-items/IconSectionC"
 import open from "./../icons/menu-open-icon.png"
 import close from "./../icons/menu-close-icon.png"
 import './../css-files/Desktop.css'
 import { useState } from "react"
 
 export  default function Desktop () {
-    const [menu,setMenu] = useState(open)
-    
+    const [menu,setMenu] = useState(close)
+    const [showmenu,setShowMenu] = useState('menu-show')
+    const [content,setContent] = useState('content-show')
     function handleClick () {
-        if (menu == open){
-            return setMenu(close)
+        if (menu == close){
+            return setMenu(open),setShowMenu('menu-show'), setContent('content-off') 
         }
-            return setMenu(open)
+            return setMenu(close),setShowMenu('menu-off'),setContent('content-show')
     }
 
     return(
@@ -23,7 +26,7 @@ export  default function Desktop () {
         <div className="nav">
             <div className="left-nav"> 
                 <h1 className="name">Cristiano Santos</h1>
-                <LinkSection/>
+                <LinkSection className='links'/>
             </div>
             <div className="right-nav">
                 <IconSection/>
@@ -33,13 +36,15 @@ export  default function Desktop () {
         <div className="content">
             <div className="menu-ribbon">
                 <button className="menu-button" onClick={handleClick} ><img className='menu-img' src={menu}/></button>
-                <div className="menu-show">
-                    <LinkSection/>
-                    <IconSection/>
+                <div className={showmenu}>
+                    <LinkSectionC/>
+                    <IconSectionC/>
                 </div>
             </div>
-            <Section1/>
-            <Section2/>
+            <div className={content}>
+                <Section1/>
+                <Section2/>
+            </div>
         </div>
     </div>
     ) 
--- 3-Portfolio/app-root/react/src/assets/components/content-items/LinkSectionC.jsx
+++ 3-Portfolio/app-root/react/src/assets/components/content-items/LinkSectionC.jsx
@@ -1,7 +1,7 @@
-import "./../../css-files/nav-styles/LinkSection.css"
+import './../../css-files/content-styles/LinkSectionC.css'
 export default function LinkSection () {
     return(
-    <div className='links'>
+    <div className='links-c'>
         <p>Section 3</p>
         <p>Section 4</p>
         <p>Section 5 </p>
