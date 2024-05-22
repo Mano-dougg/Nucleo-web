695a8891fb72dbdec167c757ebbbe490d47c4a4a
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-05-22 16:16:25 -0300

    feat: add as rotas dos botões da Pag2 e da Pag3

--- 4-JogoDaForca/src/components/Pag2/Pag2.tsx
+++ 4-JogoDaForca/src/components/Pag2/Pag2.tsx
@@ -1,5 +1,6 @@
 import styles from './Pag2.module.css'
 import exclamation from './imgPag2/bi_exclamation-circle-fill.png'
+import { Link } from 'react-router-dom'
 
 export const Pag2 = () => {
     return(
@@ -14,12 +15,16 @@ export const Pag2 = () => {
                     <p className={styles.pCinza}> Máx. de 8 letras </p>
                 </aside>
                 <aside className={styles.asideButtons}>
+                    <Link to='/pag3'>
                     <div className={styles.buttonM1}>
                         <p className={styles.pBranco}> Salvar e Começar </p>
                     </div>
+                    </Link>
+                    <Link to='/pag1'>
                     <div className={styles.buttonM2}>
                         <p> Cancelar </p>
                     </div>
+                    </Link>
                 </aside>
             </article>
         </section>
--- 4-JogoDaForca/src/main.tsx
+++ 4-JogoDaForca/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Pag1 } from './components/Pag1/Pag1.tsx'
 import { Pag2 } from './components/Pag2/Pag2.tsx'
+import { Pag3 } from './components/Pag3/Pag3.tsx'
 
 export const Routes = createBrowserRouter ([
   {
@@ -18,6 +19,10 @@ export const Routes = createBrowserRouter ([
         {
           path: '/pag2',
           element: <Pag2/>,
+        },
+        {
+          path: '/pag3',
+          element: <Pag3/>
         }
       ]
   }
