e89dd8739fdee9434c5f41df8e2496994ed3ec2b
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-05-22 19:25:26 -0300

    feat: estrutura de arquivos pras letras da Pag3

---
+++ 4-JogoDaForca/src/components/Pag3/HangmanWord/HangmanWorld.tsx
@@ -0,0 +1,9 @@
+import styles from './HangmanWorld.module.css'
+
+export const HangmanWorld = () => {
+    return(
+        <>
+        <section className={styles.wrapper}></section>
+        </>
+    )
+}
