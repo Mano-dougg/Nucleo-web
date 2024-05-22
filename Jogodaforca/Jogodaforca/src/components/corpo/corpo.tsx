0b6a1bdefa9f42976f4711b753109e5d7cf4ed22
David <151107969+EnderZerobl@users.noreply.github.com>
2024-05-21 22:12:57 -0300

    Feat:Criação dos caminhos e início da página 2

--- Jogodaforca/Jogodaforca/src/components/corpo/corpo.tsx
+++ Jogodaforca/Jogodaforca/src/components/corpo/corpo.tsx
@@ -1,9 +1,16 @@
 import styles3 from './corpo.module.css'
+import excl from '/excl.svg'
+
 
 function Corpo(){
     return(
         <div className={styles3.div}>
             <h1>Digite uma palavra ou frase</h1>
+            <textarea></textarea>
+            <figure className="exla">
+                <img src={excl}/>
+                <h2 className="me">Máx. de 8 letras</h2>
+            </figure>
         </div>
     )
 }
