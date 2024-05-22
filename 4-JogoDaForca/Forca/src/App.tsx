174379afae085aab39485d0a86ffdc3c766757d6
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-22 15:35:00 -0300

    fix: Implementada Ilustração da Forca

--- 4-JogoDaForca/Forca/src/App.tsx
+++ 4-JogoDaForca/Forca/src/App.tsx
@@ -1,28 +1,40 @@
 import { useState } from "react"
+import React from "react"
 import Header from "./components/Header"
 import DesenhoForca from "./components/DesenhoForca"
 import PalavraForca from "./components/PalavraForca"
 import TecladoForca from "./components/TecladoForca"
 
-function App() {
+const App: React.FC = () => {
   const [palavra, setPalavra] = useState("exemplo")
   const [chutes, setChutes] = useState<string[]>([])
+  const [erros, setErros] = useState(0);
+
+  const maisErro = () => {
+    setErros(errosAntes => (errosAntes < 6 ? errosAntes+1 : errosAntes));
+  }
+  const menosErro = () => {
+    setErros(errosAntes => (errosAntes > 0 ? errosAntes-1 : errosAntes));
+  }
+
   return (
-    <body>
+    <>
       <Header />
-      <div className="resultado">Waow!😀☹️</div>
-      <DesenhoForca />
-      <PalavraForca />
-      <TecladoForca />
-      <div className="doisBotoes">
-        <button className="botaoPrim">
-          Novo jogo
-        </button>
-        <button className="botaoSec">
-          Desistir
-        </button>
-      </div>
-    </body>
+      <main>
+        <div className="resultado">Waow!😀☹️</div>
+        <DesenhoForca erros={erros} />
+        <PalavraForca />
+        <TecladoForca />
+        <div className="doisBotoes">
+          <button className="botaoPrim" onClick={menosErro}>
+            Novo jogo - DEBUG: DIMINUI ERROS
+          </button>
+          <button className="botaoSec" onClick={maisErro}>
+            Desistir - DEBUG: AUMENTA ERROS
+          </button>
+        </div>
+      </main>
+    </>
   )
 }
 
--- 4-JogoDaForca/Forca/src/components/DesenhoForca.tsx
+++ 4-JogoDaForca/Forca/src/components/DesenhoForca.tsx
@@ -1,8 +1,22 @@
-function DesenhoForca() {
+import Forca0 from '../assets/forca0.svg'
+import Forca1 from '../assets/forca1.svg'
+import Forca2 from '../assets/forca2.svg'
+import Forca3 from '../assets/forca3.svg'
+import Forca4 from '../assets/forca4.svg'
+import Forca5 from '../assets/forca5.svg'
+import Forca6 from '../assets/forca6.svg'
+import React  from 'react'
+
+type DesenhoProps = {
+    erros: 0|1|2|3|4|5|6;
+}
+
+const DesenhoForca: React.FC<DesenhoProps> = ({erros}) => {
+    
+    const imagens = [Forca0, Forca1, Forca2, Forca3, Forca4, Forca5, Forca6];
+
     return (
-        <div>
-            
-        </div>
+        <img src={imagens[erros]} alt={`Forca contando ${erros} erros de 6`} />
     );
 }
 
