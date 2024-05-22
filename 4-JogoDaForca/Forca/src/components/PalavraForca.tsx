9857acf4f03f5dae778b555a0498df11a983a7e9
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-22 18:43:45 -0300

    fix: Implementada palavra teste para forca

--- 4-JogoDaForca/Forca/src/components/PalavraForca.tsx
+++ 4-JogoDaForca/Forca/src/components/PalavraForca.tsx
@@ -1,7 +1,15 @@
 function PalavraForca() {
+    const palavra = "teste"
+    const chutes = ["t", "s", "p"]
     return (
-        <div>
-            
+        <div className="palavraEscolhida">
+            {palavra.split("").map((letra, index) => {
+                return <span style={{borderBottom: ".12em solid var(--primario)"}} key={index}>
+                    <span style={{
+                        visibility: chutes.includes(letra) ? "visible" : "hidden"
+                    }}>{letra}</span>
+                </span>
+            })}
         </div>
     );
 }
