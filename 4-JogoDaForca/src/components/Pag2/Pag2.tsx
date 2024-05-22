988bff9b923cfcf9668eb630cb1e5661abd19abe
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-05-22 11:29:56 -0300

    feat: add exclamação e texto acima dos botões da Pag2

--- 4-JogoDaForca/src/components/Pag2/Pag2.tsx
+++ 4-JogoDaForca/src/components/Pag2/Pag2.tsx
@@ -1,4 +1,5 @@
 import styles from './Pag2.module.css'
+import exclamation from './imgPag2/bi_exclamation-circle-fill.png'
 
 export const Pag2 = () => {
     return(
@@ -8,12 +9,18 @@ export const Pag2 = () => {
                 <input type="text" name = "Frase" placeholder = "Digite uma palavra ou frase"  id = 'Frase' required/>
             </article>
             <article className={styles.articleButtons}>
-                <div className={styles.buttonM1}>
-                    <p className={styles.pBranco}> Salvar e Começar </p>
-                </div>
-                <div className={styles.buttonM2}>
-                    <p> Cancelar </p>
-                </div>
+                <aside className={styles.asideExclamation} >
+                    <img src={exclamation} alt="Simbolo de exclamação" />
+                    <p className={styles.pCinza}> Máx. de 8 letras </p>
+                </aside>
+                <aside className={styles.asideButtons}>
+                    <div className={styles.buttonM1}>
+                        <p className={styles.pBranco}> Salvar e Começar </p>
+                    </div>
+                    <div className={styles.buttonM2}>
+                        <p> Cancelar </p>
+                    </div>
+                </aside>
             </article>
         </section>
         </>
