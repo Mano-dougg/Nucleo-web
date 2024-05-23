54c12c160dc909bfaf1fad90ecad9ff29f281622
EderNatan.dev <edernatan124@gmail.com>
2024-05-22 21:25:58 -0300

    create puppet

--- 4-JogoDaForca/jogo-da-forca/src/components/hangman-drawing.tsx
+++ 4-JogoDaForca/jogo-da-forca/src/components/hangman-drawing.tsx
@@ -1,7 +1,108 @@
-import React from "react";
+import styled from "styled-components";
 
-export default function HangmanDrawing(){
-    return(
-        <div>Hangman-drawing</div>
-    )
+const Base = styled.div`
+    height: 2px;  
+    width: 250px;
+    background:white;
+    margin-left: 80px;
+`;
+const VerticalLine = styled.div`
+    height: 250px;  
+    width: 2px;
+    background:white;
+    margin-left: 150px;
+`;
+const HorizontalLine = styled.div`
+    height: 2px;  
+    width: 140px;
+    background:white;
+    position: absolute;
+    right: 60px;
+    top:0;
+`;
+
+const Small = styled.div`
+    height: 40px;  
+    width: 2px;
+    background:white;
+    margin-left: 150px;
+    position: absolute;
+    top: 0;
+    right:60px;
+`;
+
+const Head = styled.div`
+    height: 30px;  
+    width: 30px;
+    border: 2px solid white;
+    border-radius: 50%;
+    position: absolute;
+    top: 40px;
+    right:45px;
+`;
+
+const Body = styled.div`
+    height: 90px;  
+    width: 2px;
+    background:white;
+    position: absolute;
+    top: 75px;
+    right:60px;
+`;
+
+const RightArm = styled.div`
+    height: 2px;  
+    width: 50px;
+    background:white;
+    position: absolute;
+    top: 90px;
+    right:15px;
+    rotate:30deg;
+`;
+
+const LeftArm = styled.div`
+    height: 2px;  
+    width: 50px;
+    background:white;
+    position: absolute;
+    top: 90px;
+    right:58px;
+    rotate:-30deg;
+`;
+
+const RightLeg = styled.div`
+    height: 2px;  
+    width: 50px;
+    background:white;
+    position: absolute;
+    top: 185px;
+    right:24px;
+    rotate:60deg;
+`;
+const LeftLeg = styled.div`
+    height: 2px;  
+    width: 50px;
+    background:white;
+    position: absolute;
+    top: 185px;
+    right: 48px;
+    rotate:-60deg;
+`;
+
+export default function HangmanDrawing() {
+    return (
+        <div style={{ position: 'relative',
+         }}>
+            <LeftLeg/>
+            <RightLeg/>
+            <LeftArm/>
+            <RightArm/>
+            <Body/>
+            <Head/>
+            <Small/>
+            <HorizontalLine/>
+            <VerticalLine/>
+            <Base />
+        </div>
+    );
 }
--- 4-JogoDaForca/jogo-da-forca/src/components/hangmanword.tsx
+++ 4-JogoDaForca/jogo-da-forca/src/components/hangmanword.tsx
@@ -1,5 +1,18 @@
-export default function HangmanWord(){
-    return(
-        <div>Hangman-word</div>
-    )
+import { styled } from "styled-components";
+
+const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+gap: 0.5rem;
+font-size: 6rem;
+text-transform: uppercase;
+font-weight: bold;
+font-family: monospace;
+
+`
+
+export default function HangmanWord() {
+    return <Wrapper>
+        wapper
+    </Wrapper>
 }
