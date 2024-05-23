2781b07a4dd1298e8783df2968663e2647dfd08f
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-23 18:17:46 -0300

    fix: Refeito header para fazer sentido

--- 4-JogoDaForca/Forca/src/components/Header.tsx
+++ 4-JogoDaForca/Forca/src/components/Header.tsx
@@ -3,9 +3,11 @@ import Imagem from '../assets/header.svg'
 function Header() {
     return (
         <header>
-            <div id='logoAzinho'>
-                <img src={Imagem} style={{fill: 'var(--primario)'}} />
-            </div>
+            <a href='https://gojiberry.neocities.org/' target='_blank'>
+                <div id='logoAzinho'>
+                    <img src={Imagem} style={{fill: 'var(--primario)'}} />
+                </div>
+            </a>
         </header>
     );
 }
