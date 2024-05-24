4b7cc25dfbbc8b148d6b100f8ef6c3c7edf90433
David <151107969+EnderZerobl@users.noreply.github.com>
2024-05-24 10:51:53 -0300

    Fix: Adição de Reset ao placar e ajustes

--- Jogodaforca/Jogodaforca/src/components/placar/placar.tsx
+++ Jogodaforca/Jogodaforca/src/components/placar/placar.tsx
@@ -1,15 +1,33 @@
 import styless from './placar.module.css'
+import baixados from '/baixados.jpg'
+import { useState } from 'react'
 
 interface needs{
     vencer : number
     perder : number
 }
 
+function Resetar(){
+    localStorage.setItem('Venci', '0');
+    localStorage.setItem('Perdi', '0');
+    window.location.reload()
+}
+      
+
 function Placar({vencer,perder}:needs){
+    const [menuO, setmenuO] = useState(true)
+    const trocar = () => {
+      setmenuO(!menuO)
+    }
     return(
     <div className={styless.div}>
-    Vitórias: {vencer || 0}
-    <span>Derrotas: {perder || 0}</span>
+    <figure>
+    <img src={baixados}/>
+    <h2>SCORE</h2>
+    <p>Vitórias: {vencer || 0}</p>
+    <span className='um'>Derrotas: {perder || 0}</span>
+    <button onClick={trocar}>Reset</button>
+    </figure>
     </div>
     )
 }
