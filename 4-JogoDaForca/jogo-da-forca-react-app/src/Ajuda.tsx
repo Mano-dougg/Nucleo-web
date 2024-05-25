15145c46b675d6f38fb07dd543d3f82f24e68255
Matheus Dias <matheuscasmd@gmail.com>
2024-05-25 02:52:37 -0300

    finalização card de ajuda

---
+++ 4-JogoDaForca/jogo-da-forca-react-app/src/Ajuda.tsx
@@ -0,0 +1,23 @@
+import './Ajuda.css'; // Arquivo de estilos para o modal
+const Ajuda = () => {
+  return (
+    <div className="ajuda-overlay">
+      <div className="ajuda-content">
+        <button className="close-button">X</button>
+        <h2>Como Jogar HANGMAN:</h2>
+        <p>As <span className='atencao'>regras</span> do jogo da forca são <span className='joia'>simples</span>:</p>
+        <ul>
+          <li>O usuário tem a opção de <span className='joia'>salvar</span> uma palavra no banco de palavras ou <span className='joia'>jogar</span> com uma palavra <span className='atencao'>temporária</span></li>
+          <br />
+          <li>Você pode adivinhar uma letra por vez e cada <span className='joia'>acerto</span> revela a letra na palavra.</li>
+          <br />
+          <li>Com 6 tentativas todo o boneco é <span className='atencao'>pendurado</span> na corda e você <span className='atencao'>perde</span></li>
+          <br />
+          <li>Acesse banco de palavras para <span className='joia'>adicionar</span> nova palavra ao vocabulário ou <span className='atencao'>resetar</span> as palavras disponíveis</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Ajuda;
--- 4-JogoDaForca/jogo-da-forca-react-app/src/Home.tsx
+++ 4-JogoDaForca/jogo-da-forca-react-app/src/Home.tsx
@@ -1,5 +1,6 @@
 import rope from '../public/rope.svg'
 import './Home.css'
+import Ajuda from './Ajuda.tsx'
 const Home = () => {
     return (
         <>
@@ -9,15 +10,16 @@ const Home = () => {
             </div>
             <div className="home">
                 <div className="bloco-texto">
-                    <h1>Nova partida</h1>
+                    <h1><a href="#" className='normal'>Nova partida</a></h1>
                 </div>
                 <div className="bloco-texto">
-                    <h1>Banco de palavras</h1>
+                    <h1><a href="#" className='normal'>Banco de palavras</a></h1>
                 </div>
                 <div className="bloco-texto">
-                    <h1>Histórico de partidas</h1>
+                    <h1><a href="#" className='normal'>Histórico de partidas</a></h1>
                 </div>
-                <p className='sobre'><a href="#">Como jogar</a></p>
+                <p className='sobre'><a href="#" /*onClick={}*/>Como jogar</a></p>
+                <Ajuda/>
             </div>
         </>
     )
