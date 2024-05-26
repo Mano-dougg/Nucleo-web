f5238f6f9b3f0fcc6581d06c44a5cd25296394e6
lucasdsmachado <briandsmachado@gmail.com>
2024-05-25 22:42:33 -0300

    feat: adciona historico de palavras e estilos

--- 4-JogoDaForca/src/components/word/Word.tsx
+++ 4-JogoDaForca/src/components/word/Word.tsx
@@ -5,15 +5,24 @@ interface Props {
   guessedLetters: string[];
 }
 
-export function Word({word, guessedLetters}: Props) {
+export function Word({ word, guessedLetters }: Props) {
+  const incorrectLetters = guessedLetters.filter((letter) => word.indexOf(letter) === -1);
+  
   return (
-    <div className={styles.wrapper}>
-      {word.split('').map((char, index) => (
-        <div className={styles.letterContainer} key={index}>
-          <span className={guessedLetters.includes(char) ? styles.letter : styles.letterHidden}>{char}</span>
-          <div className={styles.letterRectangle} />
-        </div>
-      ))}
+    <div className={styles.lettersContainer}>
+      <div className={styles.wrapper}>
+        {word.split('').map((char, index) => (
+          <div className={styles.letterContainer} key={index}>
+            <span className={guessedLetters.includes(char) ? styles.letter : styles.letterHidden}>{char}</span>
+            <div className={styles.letterRectangle} />
+          </div>
+        ))}
+      </div>
+      <div className={styles.incorrectLettersContainer}>
+        {incorrectLetters.length > 0 && incorrectLetters.map((letter, index) => (
+          <span key={index} className={styles.incorrectLetter}>{letter}</span>
+        ))}
+      </div>
     </div>
   )
 }
