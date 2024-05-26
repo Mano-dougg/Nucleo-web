ac6a99e131afa8ae31102cb65b052b66b5c41f75
Matheus Dias <matheuscasmd@gmail.com>
2024-05-26 07:11:37 -0300

    finalização da interface de acesso ao jogo

--- 4-JogoDaForca/jogo-da-forca-react-app/src/JogoTemp.tsx
+++ 4-JogoDaForca/jogo-da-forca-react-app/src/JogoTemp.tsx
@@ -8,6 +8,7 @@ interface JogoTempProps {
 const JogoTemp: React.FC<JogoTempProps> = ({ show, onClose }) => {
     const [input, setInput] = useState('');
     const [enviado, setEnviado] = useState(false);
+    const [execucao,setExecucao] = useState(false);
 
     const handleKey = (event : React.KeyboardEvent<HTMLTextAreaElement>) => {
       if (event.key === 'Enter') {
@@ -20,24 +21,31 @@ const JogoTemp: React.FC<JogoTempProps> = ({ show, onClose }) => {
       setInput(event.target.value);
     };
 
+    const ligarJogo = () => {
+      setExecucao(!execucao);
+    }
+
     if (!show) return null;
 
     return(
-            <div className="container-jogo">
-              <button className="retorno" onClick={onClose}>X</button>
-              <div className="pergunta">
-                <p>Digite a palavra escolhida abaixo:</p>
-                {!enviado ? (
-                  <textarea
-                    className="entrada-palavra"
-                    value={input}
-                    onChange={handleChange}
-                    onKeyDown={handleKey}
-                  />) : (
-                  <p>Você digitou: {input}</p>
-                    )}
-              </div>
+      <div className="interface" style={{display: !execucao ? 'block' : 'none'}}>
+          <div className="container-jogo">
+            <button className="retorno" onClick={onClose}>X</button>
+            <div className="pergunta">
+              <p>Digite a palavra escolhida abaixo:</p>
+              {!enviado ? (
+                <textarea
+                  className="entrada-palavra"
+                  value={input}
+                  onChange={handleChange}
+                  onKeyDown={handleKey}
+                />) : (
+                <p>Você digitou: {input}</p>
+                  )}
             </div>
+            <p className="aviso" style={{display: enviado ? 'block' : 'none'}}><button className="botao" onClick={ligarJogo}>Clique aqui para jogar</button> <br /> F5 para escolher outra palavra</p>
+          </div>
+      </div>
         )
 }
 export default JogoTemp;
