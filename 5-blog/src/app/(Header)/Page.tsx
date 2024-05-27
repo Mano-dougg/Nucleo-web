be271ade14d27337526e4b1f8cc134dfb6606bbd
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-05-27 20:20:45 -0300

    feat: create component Header

---
+++ 5-blog/src/app/(Header)/Page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Header() {
+    return (
+      <>
+      <h1>Header</h1>
+      <h2>footer</h2>
+      </>
+    );
+}
+  
---
+++ 5-blog/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "./(Header)/Page";
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <h1>Projeto Criado</h1>
+    </>
+  );
+}
