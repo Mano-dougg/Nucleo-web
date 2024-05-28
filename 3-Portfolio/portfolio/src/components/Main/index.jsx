a3263ef91c627060e384735997559ab6510d2001
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-05-28 20:16:56 -0300

    commit inicial: configuração e organização de pastas e variáveis

--- 3-Portfolio/portfolio/src/components/Main/index.jsx
+++ 3-Portfolio/portfolio/src/components/Main/index.jsx
@@ -1,11 +1,21 @@
 import './style.css'
 import Header from "./Header"
 import Section from "./Section"
+import { useFirstPrismicDocument} from '@prismicio/react'
+
 
 export default function Main(){
+
+    const [document] = useFirstPrismicDocument('home');
+    
+    console.log(document) 
+
+    //console.log(document.data)
     return(
         <main>
+            
             < Header />
+            <p></p>
             <Section></Section>
         </main>
     )
--- 3-Portfolio/portfolio/src/main.jsx
+++ 3-Portfolio/portfolio/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { PrismicProvider } from "@prismicio/react";
+import { client } from "./services/prismic.ts";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <PrismicProvider client={client}>
+      <App />
+    </PrismicProvider>
+  </React.StrictMode>
+);
