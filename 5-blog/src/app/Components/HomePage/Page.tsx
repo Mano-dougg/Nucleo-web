25fc16cfc4d1654c3cf9a55029d1f6cdb2474ea0
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-05-28 16:13:50 -0300

    feat: styling HomePage

--- 5-blog/src/app/Components/HomePage/Page.tsx
+++ 5-blog/src/app/Components/HomePage/Page.tsx
@@ -1,24 +1,31 @@
 import astrounaut from './imgHome/Astronaut.png'
 import Image from 'next/image'
+import styles from './Page.module.css'
 
 export default function HomePage(){
     return(
         <>
-        <section>
+        <section className={styles.homePage}>
             <article>
                 <h1>Sua dose diária de informação</h1>
             </article>
-            <article>
-                <div>
+            <article className={styles.mainContent}>
+                <div className={styles.textMainContent}>
                     <h2>Incrível crescimento das IAs</h2>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere, turpis cursus elementum commodo,
-                        lorem leo dignissim mi, eget tempus elit metus vel libero. Quisque finibus, tellus nec tempor cursus,
-                        nisi lorem vulputate quam, eu sodales orci ligula et metus. Ut eget posuere metus. Mauris porttitor consequat eros,
-                        nec porttitor libero egestas sed. Nullam laoreet enim ac risus semper convallis. Integer efficitur viverra purus,
-                        a bibendum magna commodo non. Aliquam tempus mauris scelerisque scelerisque vestibulum. Curabitur metus libero,
-                        ullamcorper a euismod a, maximus non felis. Praesent eget est venenatis, malesuada ipsum non, lacinia lorem. 
-                        Proin malesuada ex luctus accumsan maximus. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                        <br /> posuere, turpis cursus elementum commodo, lorem leo 
+                        <br /> dignissim mi, eget tempus elit metus vel libero. Quisque
+                        <br /> finibus, tellus nec tempor cursus, nisi lorem vulputate
+                        <br /> quam, eu sodales orci ligula et metus. Ut eget posuere 
+                        <br /> metus. Mauris porttitor consequat eros, nec porttitor
+                        <br /> libero egestas sed. Nullam laoreet enim ac risus semper
+                        <br /> convallis. Integer efficitur viverra purus, a bibendum
+                        <br /> magna commodo non. Aliquam tempus mauris scelerisque
+                        <br /> scelerisque vestibulum. Curabitur metus libero,
+                        <br />ullamcorper a euismod a, maximus non felis. Praesent eget
+                        <br /> est venenatis, malesuada ipsum non, lacinia lorem. Proin
+                        <br /> malesuada ex luctus accumsan maximus. 
                     </p>
                 </div>
                 <div>
