f79013f82fe635acc6b960f9b0aede03229fd4e9
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-05-27 21:36:54 -0300

    feat: add images in Header

--- 5-blog/src/app/Components/(Header)/Page.tsx
+++ 5-blog/src/app/Components/(Header)/Page.tsx
@@ -1,5 +1,6 @@
-
-
+import Sun from './imgHeader/Sun.png'
+import Search from './imgHeader/Search.png'
+import Image from 'next/image';
 
 export default function Header() {
     return (
@@ -9,12 +10,22 @@ export default function Header() {
             <h1> <span>AI</span>TODAY</h1>
           </section>
           <section>
-            <article className={SearchBar}>
+            <article>
               <h2>Pesquisar</h2>
-              <img src="" alt="" />
+              <Image
+                src={Search}
+                width={32}
+                height={32}
+                alt="Search Bar icon"
+              />
             </article>
             <article>
-              <img src="" alt="" />
+            <Image
+                src={Sun}
+                width={32}
+                height={32}
+                alt="Picture of a Sun"
+              />
             </article>
           </section>
       </header>
