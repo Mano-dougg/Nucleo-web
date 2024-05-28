04acb6862534fc0f38452a58a3e39a4c1e782db5
Rian Victor <rianvictorribeiro@gmail.com>
2024-05-28 20:11:24 -0300

    feat: start of the project

---
+++ 5-Blog/blog-film/src/app/layout.tsx
@@ -0,0 +1,31 @@
+
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+import "../app/css/globals.css";
+
+import { Inter, Montserrat} from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+ 
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat',
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="">{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
