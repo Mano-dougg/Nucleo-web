33d09e1c0369e14a33c8cf6c15a0852737b42772
Matheus Dias <matheuscasmd@gmail.com>
2024-05-31 20:56:03 -0300

    cards maiores do carousel

--- 5-Blog/blog-next/src/app/cards1.tsx
+++ 5-Blog/blog-next/src/app/cards1.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
+import styles from "./modulos.module.css";
 const Card = () => {
     return(
-        <div className="card">
+        <div className={styles.card}>
             <h2>Notícia muito importante</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere, turpis cursus elementum commodo, lorem leo dignissim mi, eget tempus elit metus vel libero.</p>
-            <div className="pessoal">
+            <div className={styles.pessoal}>
                 <Image
                 src="/fulano.svg"
                 width={44}
                 height={44}
                 alt="fulano"
                 />
-                <div className="descricao">
+                <div className={styles.descricao}>
                     <p>Fulano das IAs</p>
                     <p>01/01/2024</p>
                 </div>
@@ -24,6 +25,12 @@ const Card = () => {
                     />
                 </button>
             </div>
+            <Image
+                src="/ai-rectangle.svg"
+                width={302}
+                height={156}
+                alt="robo de IA"
+                />
         </div>
     )
 }
