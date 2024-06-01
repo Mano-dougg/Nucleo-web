e347435d95f4492b756815b0d585a8b9ac0e8d8d
Matheus Dias <matheuscasmd@gmail.com>
2024-05-31 23:43:42 -0300

    ajustes

--- 5-Blog/blog-next/src/app/page.tsx
+++ 5-Blog/blog-next/src/app/page.tsx
@@ -13,11 +13,12 @@ export default function Home() {
   return (
     <main>
       <PageHeader></PageHeader>
+      <h1 className="subtitulo">Sua dose diária de informação</h1>
       <br />
-      <h1>Ultimas postagens</h1>
+      <h1 className="subtitulo">Ultimas postagens</h1>
       <Card1></Card1>
       <br />
-      <h1>Mais acessadas</h1>
+      <h1 className="subtitulo">Mais acessadas</h1>
       <Card2></Card2>
       <br />
       <PageFooter></PageFooter>
