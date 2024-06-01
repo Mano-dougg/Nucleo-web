2cb39a7434d3da3d8802af4257ae8a9721bca9c2
Beatriz Pereira Espinheira <beatrizespinheiraa@gmail.com>
2024-06-01 12:21:48 -0300

    feat: cria nova seção de cards responsiva

--- 5-Blog/projeto-blog/src/components/card/Card.tsx
+++ 5-Blog/projeto-blog/src/components/card/Card.tsx
@@ -12,16 +12,16 @@ export default function Card (){
         <section className="card">
             <h1>Notícia muito importante</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere, turpis cursus elementum commodo, lorem leo dignissim mi, eget tempus elit metus vel libero.</p>
-                <div>
-                    <Link href="\creator" className="autor-info">
-                        <Image src={ProfilePic} alt="" className="profile-img"></Image>
-                        <section>
-                            <h2>Fulano das IAs</h2>
-                            <p>01/01/2024</p>
-                        </section>
-                    </Link>
-                    <Image src={Share} alt="share" className="share-img"></Image>
-                </div>
-                <Image src={CardImg} alt="robot" className="robot-img"></Image>
+            <div>
+                <Link href="\creator" className="autor-info">
+                    <Image src={ProfilePic} alt="" className="profile-img"></Image>
+                    <section>
+                        <h2>Fulano das IAs</h2>
+                        <p>01/01/2024</p>
+                    </section>
+                </Link>
+                <Image src={Share} alt="share" className="share-img"></Image>
+            </div>
+            <Image src={CardImg} alt="robot" className="robot-img"></Image>
         </section>
         )};
