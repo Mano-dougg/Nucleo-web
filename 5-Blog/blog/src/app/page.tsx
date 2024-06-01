583f4fe4ae80e618be4a6dbdfa70b267bba19fbd
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-05-31 23:34:53 -0300

    fix: tentando adicionar rodapé e cabeçalho. Falhando?

--- 5-Blog/blog/src/app/page.tsx
+++ 5-Blog/blog/src/app/page.tsx
@@ -1,7 +1,12 @@
+import Cabecalho from "@/components/cabecalho"
+import Rodape from "@/components/rodape"
+
 export default function Home(){
     return (
         <>
+            <Cabecalho />
             <h1>Teste</h1>
+            <Rodape />
         </>
     )
 }
