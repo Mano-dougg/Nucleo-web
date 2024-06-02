440a9c111433d9b93e8421c99048f521318ef511
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-06-02 16:35:06 -0300

    retirando prismic do projeto portofolio

--- 3-Portfolio/portfolio/src/components/Main/index.jsx
+++ 3-Portfolio/portfolio/src/components/Main/index.jsx
@@ -1,16 +1,11 @@
 import './style.css'
 import Header from "./Header"
 import Section from "./Section"
-import { useFirstPrismicDocument} from '@prismicio/react'
 
 
 export default function Main(){
-
-    const [document] = useFirstPrismicDocument('home');
     
     console.log(document) 
-
-    //console.log(document.data)
     return(
         <main>
             
--- 3-Portfolio/portfolio/src/services/prismic.ts
+++
@@ -1,5 +0,0 @@
-import * as Prismic from '@prismicio/client'
-
-export const client = Prismic.createClient("jm-repository",{
-    accessToken:'MC5abFI1bUJBQUFDSUEzZzhw.77-9P19u77-9LntOHe-_vTQy77-977-9dl_vv73vv70ldu-_ve-_ve-_vRNwVu-_ve-_ve-_ve-_ve-_ve-_vQ'
-})
