481cce08e6f13e76d8dae2ee315be0039e5c4719
Mauricio Matchal <mauriciomatchal@gmail.com>
2024-06-02 11:11:53 -0300

    cor do butão

--- 5-Blog/blog/src/components/Button.tsx
+++ 5-Blog/blog/src/components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({
   return (
     <PrismicNextLink
       className={clsx(
-        "block w-fit bg-lime-700 hover:bg-lime-800 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider",
+        "block w-fit bg-lime-600 hover:bg-lime-700 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider",
         className
       )}
       {...restProps}
