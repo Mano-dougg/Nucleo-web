16ef3d6a291a6c830c8a4001c8ca5d9865e17075
samuelalmeida <samueldealmeidadossantos@gmail.com>
2024-06-04 15:14:44 -0300

    feat: integracao do prisma e do banco relacional ao projeto

--- 6-CrudMembros/src/server.ts
+++ 6-CrudMembros/src/server.ts
@@ -1,15 +1,28 @@
+import { PrismaClient } from "@prisma/client"
+import { configDotenv } from "dotenv"
 import express, { Request, Response } from "express"
 
+
+configDotenv()
 
 const app = express()
 const port = 3000
 
-app.
-get('/teste', (req: Request, res: Response) => {
-    console.log(req.params)
-   res.send ({
-       teste: 'Hello!'
-   })
+const prisma = new PrismaClient()
+
+//criar usuario Post
+app.post('/user', async(req: Request, res: Response) =>{
+    const user = await prisma.user.create({
+        data: {
+            name: 'Samuel' ,
+            email: 'samuel@ufba',
+            idade: 19,
+            estado: 'bahia', 
+            senha: '1234sadxc',
+            cidade: 'salvador'
+        }
+    })
+    res.send(user)
 })
 
 app.listen(port, () => {
