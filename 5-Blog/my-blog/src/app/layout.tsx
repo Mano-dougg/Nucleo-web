f23383d3e157984f2b76ac1d4fd6280557b2e7b2
talitaester <talita.alves@ufba.br>
2024-06-04 18:54:17 -0300

    modified:   5-Blog/my-blog/src/app/layout.tsx

--- 5-Blog/my-blog/src/app/layout.tsx
+++ 5-Blog/my-blog/src/app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from "next";
-import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
 import clsx from "clsx";
-import { createClient } from "@/prismicio";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import { createClient, repositoryName } from "@/prismicio";
+import { PrismicPreview } from "@prismicio/next";
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { PrismicPreview } from "@prismicio/next";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -20,34 +21,33 @@ const nunitoSans = Nunito_Sans({
 });
 
 export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
 
- const client = createClient();
-
- const settings = await client.getSingle("settings")
-
+  const settings = await client.getSingle("settings");
 
   return {
-    title: settings.data.site_title || "i'm sorry, we're out",
-    description: settings.data.meta_description || "promise we'll be back soon",
+    title: settings.data.site_title || "Flowrise",
+    description:
+      settings.data.meta_description || "Flowrise is the relaxing app for you.",
     openGraph: {
       images: [settings.data.og_image.url || ""],
     },
-  }
+  };
 }
- 
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
       <body>
-        <Header/>
+        <Header />
         {children}
-        <Footer/>
+        <Footer />
         <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50" />
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
