2079e0e6dad6c4fecdb5990216a3f0ad91b667bf
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-06-05 10:36:15 -0300

    feat: rota post, validações para os campos de nome, cidade, estado e idade

--- 6-CrudMembros/src/crud.ts
+++ 6-CrudMembros/src/crud.ts
@@ -1,16 +1,27 @@
 import { Request, Response, Router } from "express";
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 
 const router = Router()
 const prisma = new PrismaClient()
 
-router.post("/", async (req:Request, res:Response) =>{
-    try{
-        const {nome, idade, email, senha, estado, cidade}:
-        {nome:string, idade:number, email:string, senha:string, estado:string, cidade:string} = req.body;
+router.post("/", async (req: Request, res: Response) => {
+    try {
+        const { nome, idade, email, senha, estado, cidade } = req.body;
+
+        if(nome.match(/[0-9]/) != null){
+            throw new Error("O nome deve conter apenas letras!")
+        }
+
+        if(estado.match(/[^\w\s]/) != null || cidade.match(/[^\w\s]/) != null){
+            throw new Error("Os campos de cidade e estado devem conter apenas letras");
+        }
+
+        if (typeof idade != "number") { 
+            throw new Error("Idade inválida");
+        }
 
         const addUser = await prisma.user.create({
-            data:{
+            data: {
                 nome: nome,
                 idade: idade,
                 email: email,
@@ -19,11 +30,14 @@ router.post("/", async (req:Request, res:Response) =>{
                 cidade: cidade
             }
         })
-    }catch(error) {
-        console.log(error)
-    }
-   
+        res.send("Usuário cadastrado!")
+        res.status(200).json(addUser)
 
+    } catch (error: any) {
+        console.log("Erro detectado " + error)
+        res.status(400).json({ msg: error });
+    }
 })
 
+export default router;
 
--- 6-CrudMembros/src/server.ts
+++ 6-CrudMembros/src/server.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import { Request, Response, Express } from "express"
+import router from "./crud";
 
 const server: Express = express();
 const PORT = 3001;
 
 server.use(express.json())
+
+server.use("/usuario", router)
 
 server.get("/", (_req: Request, res:Response )=>{
     res.send("Primeira rota")
