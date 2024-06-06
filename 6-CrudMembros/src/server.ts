cd3df2469f7af9d3d6114a39ac41e1ecf0007145
Matheus Dias <matheuscasmd@gmail.com>
2024-06-06 19:03:47 -0300

    ajustes

--- 6-CrudMembros/src/server.ts
+++ 6-CrudMembros/src/server.ts
@@ -1,14 +1,33 @@
-import { Request,Response } from "express"
+import { PrismaClient } from "@prisma/client"
+import { configDotenv } from "dotenv";
+import express,{ Request,Response } from "express"
 
-const express = require('express')
-const app = express()
-const port = 3000
+configDotenv()
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+const app = express();
+const port = 3000;
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get('/teste', (req: Request, res: Response) => {
+  console.log(req.query)
+  res.send({
+    teste: 'hello'
+  })
+})
+
+app.post('/user', async (req:Request, res:Response) => {
+  const user = await prisma.user.create({
+    data: {
+      nome: 'Matheus',
+      email:  'matheus@info.br',
+      idade: 22
+    }
+  })
+  res.send(user);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Servidor rodando na porta ${port}`);
+});
