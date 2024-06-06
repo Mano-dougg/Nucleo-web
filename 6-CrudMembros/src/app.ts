0cd92d1c8bc0c2cd2bfe16cb26e9c6be00723b15
David <151107969+EnderZerobl@users.noreply.github.com>
2024-06-06 10:23:34 -0300

    Feat:Métodos implementados

--- 6-CrudMembros/src/app.ts
+++ 6-CrudMembros/src/app.ts
@@ -4,6 +4,7 @@ import { openDb } from './organiza';
 import { configDotenv } from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 import usuar from './conexoes/usuar';
+import router from './rotas/rt';
 
 const app = express();
 const prisma = new PrismaClient();
@@ -12,13 +13,19 @@ configDotenv()
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
+/* app.get('/', (req: Request, res: Response) => {
     return res.status(200).json('Olá Mundo! Essa é a minha primeira rota :)');
 });
 
 app.post('/usuarios', async (req: Request, res: Response) => {
     const { nome, email, senha, idade, estado, cidade } = req.body;
 
+    const usuarioExistente = await prisma.user.findUnique({where: { email }});
+
+    if (usuarioExistente) {
+        return res.status(400).json({ error: 'Usuário com este email já existe' });
+    }
+    
     try {
         const novoUsuario = await prisma.user.create({
             data: {
@@ -36,7 +43,30 @@ app.post('/usuarios', async (req: Request, res: Response) => {
     }
 });
 
-const PORT = 3000;
+app.get('/usuarios/:email',  async (req: Request, res: Response)=> {
+    const {email} = req.params
+
+    const us = await prisma.user.findUnique({where: { email }});
+
+    try{
+        if(us){
+            return res.json(us)
+        }
+        else{
+            res.status(400).json({ error: 'Usuário não encontrado' })
+        }
+    }
+    catch (error) {
+        res.status(400).json({ error: 'Erro para achar usuário' });
+    }
+
+})
+
+app. */
+
+app.use('/', router);
+
+const PORT = 4005;
 
 app.listen(PORT, () => {
     console.log(`Servidor está executando na porta ${PORT}`);
---
+++ 6-CrudMembros/src/rotas/rt.ts
@@ -0,0 +1,140 @@
+import { Router, Request, Response } from 'express';
+import { prisma } from '../app';
+import { request } from 'http';
+
+const router = Router();
+
+
+// CRUD BÁSICO
+
+router.post('/usuarios', async (req: Request, res: Response) => {
+    const { nome, email, senha, idade, estado, cidade } = req.body;
+
+    const usuarioExistente = await prisma.user.findUnique({where: { email }});
+
+    if (usuarioExistente) {
+        return res.status(400).json({ error: 'Usuário com este email já existe' });
+    }
+    
+    try {
+        const novoUsuario = await prisma.user.create({
+            data: {
+                nome,
+                email,
+                senha,
+                idade,
+                estado,
+                cidade
+            }
+        });
+        res.status(201).json(novoUsuario);
+    } catch (error) {
+        res.status(400).json({ error: 'Erro ao criar usuário' });
+    }
+});
+
+router.get('/usuarios/emails/:email',  async (req: Request, res: Response)=> {
+    const { email } = req.params;
+
+    const us = await prisma.user.findUnique({where: { email }});
+
+    try {
+        if (us) {
+            return res.json(us);
+        } else {
+            res.status(400).json({ error: 'Usuário não encontrado' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: 'Erro para achar usuário' });
+    }
+});
+
+router.get('/usuarios/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const us = await prisma.user.findUnique({ where: { id: Number(id) } });
+
+        if (us) {
+            return res.json(us);
+        } else {
+            res.status(404).json({ error: 'Usuário não encontrado' }); 
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar usuário' }); 
+    }
+});
+
+router.delete('/usuarios/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const us = await prisma.user.findUnique({ where: { id: Number(id) } });
+        if (us) {
+            const deletado = await prisma.user.delete({ where: { id: Number(req.params.id) } })
+            return res.json(deletado);
+        } else {
+            res.status(404).json({ error: 'Quer terminar com o que nem começou?' }); 
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar usuário' }); 
+    }
+});
+
+router.put('/usuarios/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;  
+    const { nome, email, senha, idade, estado, cidade } = req.body;
+
+
+    try {
+        const us = await prisma.user.findUnique({ where: { id: Number(id) } });
+
+        if (us) {
+            if(email){
+                const usuarioExistente = await prisma.user.findUnique({where: { email }})
+                if (usuarioExistente && usuarioExistente.id !== Number(id)) {
+                    return res.json({ error: 'Parado aí, esse email já existe' });
+                }
+            }
+            const updates = await prisma.user.update({
+                where: { id: Number(id) },  
+                data: {
+                    nome,
+                    email,
+                    senha,
+                    idade,
+                    estado,
+                    cidade
+                }
+            });
+            return res.json(updates);  
+        } else {
+            res.status(404).json({ error:'Acho que você quer atualizar demais' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao atualizar usuário' });
+    }
+});
+
+// Buscas Específicas
+
+router.get('/usuarios/salvos/:nome', async(req:Request, res:Response) => {
+    const {nome} = req.params;
+    const us = await prisma.user.findMany({where: { nome }});
+    try{
+        if (us.length > 0){
+            return res.json(us)
+        }
+        else{
+            return res.status(404).json("Nada salvo aqui")
+        }
+    }
+    catch(error){
+        res.status(500).json("Erro ao procurar nome")
+    }
+});
+
+
+
+
+export default router;
