5744f34c6fb199b20b150d3f0fb2f10b73bbd097
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-06-07 15:23:07 -0300

    feat: Implementados CSS, aplicação dos itens na página de postagem

--- 5-Blog/blog/src/app/autor/page.tsx
+++ 5-Blog/blog/src/app/autor/page.tsx
@@ -1,3 +1,15 @@
+import Cabecalho from '@/components/Cabecalho/cabecalho'
+import Rodape from '@/components/Rodape/rodape'
+import './style.css'
+
 export default function Autor(){
-    return <h1>Sobre mim</h1>
+    return (
+        <div className='pagina'>
+            <Cabecalho />
+            <main>
+                <h1>Sobre mim</h1>
+            </main>
+            <Rodape />
+        </div>
+)
 }
--- 5-Blog/blog/src/app/postagem/page.tsx
+++ 5-Blog/blog/src/app/postagem/page.tsx
@@ -1,20 +1,37 @@
 import Cabecalho from "@/components/Cabecalho/cabecalho"
 import { createClient } from "../../prismicio"
 import Rodape from "@/components/Rodape/rodape"
+import './style.css'
+import { PrismicRichText } from "@prismicio/react"
+import { PrismicNextImage } from "@prismicio/next"
 
 export default async function Post(){
 
     const prismic = createClient()
-    const post = await prismic.getSingle('postagem')
-    console.log(post)
+    const post = await prismic.getByUID('postagem', 'post')
 
     return (
-        <>
+        <div className="pagina">
             <Cabecalho />
             <main>
-                <h1>Post</h1>
+                <h1>{post.data.titulo}</h1>
+                <div style={{display:"flex", gap:"32px"}}>
+                    <div style={{display:"flex", flexDirection:"column", gap:"32px"}}>
+                        <PrismicRichText field={post.data.texto1} />
+                    </div>
+                    <PrismicNextImage field={post.data.imagem1} />
+                </div>
+                <PrismicRichText field={post.data.texto2} />
+                <div style={{display:"flex", gap:"32px"}}>
+                    <PrismicNextImage field={post.data.imagem2} />
+                    <PrismicNextImage field={post.data.imagem3} />
+                </div>
+                <PrismicRichText field={post.data.texto3} />
+                <div className="autor"></div>
+                <h1>Mais postagens</h1>
+                {/* CARROSSEL DE POSTS HORIZONTAL */}
             </main>
             <Rodape />
-        </>
+        </div>
     )
 }
