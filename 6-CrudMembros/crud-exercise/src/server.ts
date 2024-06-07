f09f8ca126458e7bb1661db123453cc0f7f638ae
Fernando Costa <costa.fernando@ufba.br>
2024-06-07 18:18:16 -0300

    feat: adds Express server

---
+++ 6-CrudMembros/crud-exercise/src/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+// Use the PORT environment variable if present in .env
+const port = process.env.EXPRESS_PORT || 3000;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Initial route.");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
