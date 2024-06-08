9be78dc0f2c4ca8046513a01bedb2fb83f7c41c1
Mikelly Correia <163700231+MiCorreia@users.noreply.github.com>
2024-06-08 20:58:47 -0300

    Alterando a cor do background e testando estrutura da page

--- 7-Lapadarie (Em Dupla)/lapadarie-front/src/app/page.tsx
+++ 7-Lapadarie (Em Dupla)/lapadarie-front/src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import style from "./page.module.css";
 
 export default function Home() {
   return (
-    <></>
+    <main className={style.corpo}>
+      <h1>Lapadarie - Eli e Mike</h1>
+
+    </main>
+    
   );
 }
