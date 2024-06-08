b3906c0d9b7ae90b166ce27d8587c98eb0b57be3
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-06-08 20:21:39 -0300

    feat: Faux-Carrosséis implementados, e responsividade completa

--- 5-Blog/blog/src/app/autor/page.tsx
+++ 5-Blog/blog/src/app/autor/page.tsx
@@ -3,12 +3,14 @@ import Rodape from '@/components/Rodape/rodape'
 import './style.css'
 import { createClient } from "../../prismicio"
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next"
-import { PrismicRichText } from "@prismicio/react"
+import Link from 'next/link'
 
 export default async function Autor(){
 
     const prismic = createClient()
     const autor = await prismic.getByUID('autor', 'autor')
+    const post1 = await prismic.getByUID('icone_dum_post', 'posticon')
+
     return (
         <div className='pagina'>
             <Cabecalho />
@@ -32,8 +34,48 @@ export default async function Autor(){
                     </div>
                 </div>
                 <p>{autor.data.texto2}</p>
-                <h1>{autor.data.mais_do_autor}</h1>
             </main>
+            <h1>{autor.data.mais_do_autor}</h1>
+            <div className="carrosselFake">
+                <div className="iconePost">
+                    <div>
+                        <Link href="/postagem">
+                            <h1>{post1.data.titulo}</h1>
+                        </Link>
+                        <p>{post1.data.descricao}</p>
+                        <div>
+                            <Link href={"/autor"}>
+                                <PrismicNextImage field={post1.data.icone_autor} />
+                                <div>
+                                    <h3>{post1.data.nome_autor}</h3>
+                                    <h4>{post1.data.data}</h4>
+                                </div>
+                            </Link>
+                            <img className="compart" src="compartigar.svg" />
+                        </div>
+                    </div>
+                    <PrismicNextImage field={post1.data.thumbnail} className="thumbnail" />
+                </div>
+                <div className="iconePost">
+                    <div>
+                        <Link href={"/postagem"}>
+                            <h1>{post1.data.titulo}</h1>
+                        </Link>
+                        <p>{post1.data.descricao}</p>
+                        <div>
+                            <Link href={"/autor"}>
+                                <PrismicNextImage field={post1.data.icone_autor} />
+                                <div>
+                                    <h3>{post1.data.nome_autor}</h3>
+                                    <h4>{post1.data.data}</h4>
+                                </div>
+                            </Link>
+                            <img className="compart" src="compartigar.svg" />
+                        </div>
+                    </div>
+                    <PrismicNextImage field={post1.data.thumbnail} className="thumbnail" />
+                </div>
+            </div>
             <Rodape />
         </div>
 )
--- 5-Blog/blog/src/app/postagem/page.tsx
+++ 5-Blog/blog/src/app/postagem/page.tsx
@@ -11,6 +11,7 @@ export default async function Post(){
     const prismic = createClient()
     const post = await prismic.getByUID('postagem', 'post')
     const autor = await prismic.getByUID('autor', 'autor')
+    const post1 = await prismic.getByUID('icone_dum_post', 'posticon')
 
     return (
         <div className="pagina">
@@ -36,9 +37,48 @@ export default async function Post(){
                         <h4>{new Date().getDay()}/{new Date().getMonth()}/{new Date().getFullYear()}</h4>
                     </div>
                 </Link>
-                <h1>Mais postagens</h1>
-                {/* CARROSSEL DE POSTS */}
             </main>
+            <h1>Mais postagens</h1>
+            <div className="carrosselFake">
+                <div className="iconePost">
+                    <div>
+                        <Link href={"/postagem"}>
+                            <h1>{post1.data.titulo}</h1>
+                        </Link>
+                        <p>{post1.data.descricao}</p>
+                        <div>
+                            <Link href={"/autor"}>
+                                <PrismicNextImage field={post1.data.icone_autor} />
+                                <div>
+                                    <h3>{post1.data.nome_autor}</h3>
+                                    <h4>{post1.data.data}</h4>
+                                </div>
+                            </Link>
+                            <img className="compart" src="compartigar.svg" />
+                        </div>
+                    </div>
+                    <PrismicNextImage field={post1.data.thumbnail} className="thumbnail" />
+                </div>
+                <div className="iconePost">
+                    <div>
+                        <Link href={"/postagem"}>
+                            <h1>{post1.data.titulo}</h1>
+                        </Link>
+                        <p>{post1.data.descricao}</p>
+                        <div>
+                            <Link href={"/autor"}>
+                                <PrismicNextImage field={post1.data.icone_autor} />
+                                <div>
+                                    <h3>{post1.data.nome_autor}</h3>
+                                    <h4>{post1.data.data}</h4>
+                                </div>
+                            </Link>
+                            <img className="compart" src="compartigar.svg" />
+                        </div>
+                    </div>
+                    <PrismicNextImage field={post1.data.thumbnail} className="thumbnail" />
+                </div>
+            </div>
             <Rodape />
         </div>
     )
