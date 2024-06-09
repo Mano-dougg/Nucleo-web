4891a2e9908f3227b742190eb73d00710c769304
Beatriz Pereira Espinheira <beatrizespinheiraa@gmail.com>
2024-06-08 21:09:49 -0300

    feat: melhora tratamento de erros e muda uso de parâmetros

--- 6-CrudMembros/user-api/src/server.ts
+++ 6-CrudMembros/user-api/src/server.ts
@@ -15,112 +15,146 @@ app.post('/users', async (req: Request, res: Response) => {
     return res.status(400).json({ error: 'Todos os dados do usuário devem ser preenchidos!' });
   }
 
-  const emailExists = await prisma.user.findUnique({where: { email }, });
+  try{
+    const emailExists = await prisma.user.findUnique({where: { email }, });
 
-  if (emailExists) {
-    return res.status(400).json({ error: 'Email já cadastrado!' });
+    if (emailExists) {
+      return res.status(400).json({ error: 'Email já cadastrado!' });
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+        age,
+        state,
+        city,
+      },
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Não foi possível criar o usuário (parâmetros inválidos)' });
   }
 
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password,
-      age,
-      state,
-      city,
-    },
-  });
-
-  res.status(201).json(user);
 });
+
 
 // 2 - rota para retornar usuario por Id
-app.get('/users', async(req: Request, res: Response) => {
-  const { id } = req.query;
+app.get('/users/:id', async(req: Request, res: Response) => {
+  const { id } = req.params;
 
-  const userFound = await prisma.user.findUnique({ where: { id: Number(id) } });
+  try{
+    const userFound = await prisma.user.findUnique({ where: { id: Number(id) } });
 
-  if (userFound){
-    return res.status(200).json(userFound);
+    if (userFound){
+      return res.status(200).json(userFound);
+    }
+
+    res.status(404).json({ error: 'Id não cadastrado' });
+  } catch (error){
+    res.status(500).json({ error: 'Id inválido' });
   }
+});
 
-  res.status(404).json({ error: 'Id não cadastrado' });
-  
-})
 
 // 3 - rota para retornar usuário por email
-app.get('/users/email', async(req: Request, res: Response) => {
-  const { email } = req.query;
+app.get('/users/email/:email', async(req: Request, res: Response) => {
+  const { email } = req.params;
 
-  const userFound = await prisma.user.findUnique({ where: { email: String(email) } });
+  try{
+    const userFound = await prisma.user.findUnique({ where: { email: String(email) } });
 
-  if (userFound){
-    return res.status(200).json(userFound);
+    if (userFound){
+      return res.status(200).json(userFound);
+    }
+
+    res.status(404).json({error: 'Email não cadastrado'})
+  } catch (error) {
+    res.status(500).json({ error: 'Email inválido' });
   }
-
-  res.status(404).json({error: 'Email não cadastrado'})
-
 })
+
 
 // 4 - rota para retornar um ou mais usuarios pelo nome
-app.get('/users/name', async(req: Request, res: Response) => {
-  const { name } = req.query;
+app.get('/users/name/:name', async(req: Request, res: Response) => {
+  const { name } = req.params;
 
-  const usersFound = await prisma.user.findMany({where: {name : String(name) }});
+  try {
+    const usersFound = await prisma.user.findMany({where: {name : String(name) }});
 
-  if (usersFound){
-    return res.status(200).json(usersFound);
+    if (usersFound.length!==0){
+      return res.status(200).json(usersFound);
+    }
+
+    return res.status(404).json({error: "Nome não cadastrado"});
+  } catch (error){
+    res.status(500).json({ error: 'Nome inválido' });
   }
-
-  return res.status(404).json({error: "Nome não cadastrado"});
 })
+
 
 // 5 - rota para deletar usuário por Id
-app.delete('/users/id', async(req: Request, res: Response) => {
-  const { id } = req.query;
+app.delete('/users/:id', async(req: Request, res: Response) => {
+  const { id } = req.params;
 
-  const userFound = await prisma.user.findUnique({ where: { id: Number ( id ) }});
+  try{
+    const userFound = await prisma.user.findUnique({ where: { id: Number ( id ) }});
 
-  if (userFound){
-    await prisma.user.delete({ where: { id: Number(id) } });
-    res.status(204).send();
+    if (userFound){
+      await prisma.user.delete({ where: { id: Number(id) } });
+      return res.status(204).json({});
+    }
+
+    return res.status(404).json({ error: "Id não cadastrado" });
+
+  } catch (error){
+      res.status(500).json({ error: "Id inválido"});
   }
-
-  return res.status(404).json({error: "Id não cadastrado"})
 })
 
-// 6 - rota para atualizar o usuario por Id (não permite caso email já cadastrado)
-app.put('/users/update', async(req: Request, res: Response) => {
-  const { id } = req.query;
-  const { name, email, password, age, state, city } = req.body;
 
-  const userFound = await prisma.user.findUnique({ where: { id: Number(id) } });
+// 6 - rota para atualizar o usuario por Id (não permite caso email já cadastrado)
+app.put('/users/:id', async(req: Request, res: Response) => {
+  const { id } =  req.params;
+  const { name, email , password, age , state , city } = req.body;
 
   if (!name || !email || !password || !age || !state || !city) {
-    return res.status(400).json({ error: 'Todos os dados do usuário devem ser preenchidos!' });
-  }
-  
-  if (!userFound) {
-    return res.status(404).json({ error: 'Id não cadastrado' });
+    return res.status(400).json({error: 'Todos os dados do usuário devem ser preenchidos!'});
   }
 
-  const emailExists = await prisma.user.findUnique({where: { email }, });
+  try {
+    const userFound =await prisma.user.findUnique({ where:{ id: Number(id)}});
 
-  if (emailExists && emailExists.id != Number(id) ) {
-    return res.status(400).json({ error: 'Email já cadastrado!' });
+    if (!userFound) {
+      return res.status(404).json({ error: 'Id não cadastrado'});
+    }
+
+    const emailExists =await prisma.user.findUnique({where: { email}, });
+
+    if (emailExists && emailExists.id != Number(id) ) {
+      return res.status(400).json({ error: 'Email já cadastrado!' });
+    }
+
+    const  updatedUser= await prisma.user.update({
+      where: { id: Number(id) },
+      data: { name, email,  password, age,  state, city }
+    });
+
+    res.status(200).json(updatedUser);
+
+  } catch (error){
+
+    res.status(500).json({ error: 'Não foi possível atualizar usuário' });
   }
-
-  const updatedUser = await prisma.user.update({
-    where: { id: Number(id) },
-    data: { name, email, password, age, state, city }
-  });
-
-  res.status(200).json(updatedUser);
 
 });
+
 
 app.listen(port, () => {
   console.log(`API rodando na porta: ${port}`);
 });
+
+
 
