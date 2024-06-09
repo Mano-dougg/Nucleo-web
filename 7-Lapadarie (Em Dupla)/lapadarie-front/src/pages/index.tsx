f173b1e0b8d1360c7a05118525ebd112ccc5c0ac
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-06-09 17:03:43 -0300

    feat: componente para conter os dados do cliente no cadastro pronto

--- 7-Lapadarie (Em Dupla)/lapadarie-front/src/pages/index.tsx
+++ 7-Lapadarie (Em Dupla)/lapadarie-front/src/pages/index.tsx
@@ -1,42 +1,9 @@
-import Card from "@/components/Card";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Fragment } from "react";
+import CardClient from "@/components/CardClient";
 
 function DialogDemo() {
   return (
     <div className="flex md:flex-row gap-4 w-full flex-col">
-      <Card
-        titulo={"Pessoas na fila"}
-        cardData={"7"} // Este valor vai receber os dados do fetch da API relativo aos dados do card
-        varianteCard={"card"}
-        varianteData={"card-data"}
-        icon={"/people.svg"}
-      />
-
-      <Card
-        titulo={"Pães vendidos"}
-        cardData={"350"}// Este valor vai receber os dados do fetch da API relativo aos dados do card
-        varianteCard={"card"}
-        varianteData={"card-data"}
-        icon={"/carrinho.svg"}
-      />
-
-      <Card
-        titulo={"Pães vendidos"}
-        cardData={`R$ 175,00`}// Este valor vai receber os dados do fetch da API relativo aos dados do card
-        varianteCard={"card2"}
-        varianteData={"card-data2"}
-        icon={"/Money.svg"}
-      />
+      <CardClient cliente={"Alexandre Shyjada Sousa"} totalPao={"10"} totalPagar={"10,00"}></CardClient>
     </div>
   );
 }
