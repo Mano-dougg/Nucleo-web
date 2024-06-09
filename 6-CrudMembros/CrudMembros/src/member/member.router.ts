da9fe26f43e4844d1c955a193e70be5a593fd187
Bruno-Emanoel <brunoemanoel.l.x@gmail.com>
2024-06-09 10:57:34 -0300

    feat: fixing the custom messages and codes of the requests

--- 6-CrudMembros/CrudMembros/src/member/member.router.ts
+++ 6-CrudMembros/CrudMembros/src/member/member.router.ts
@@ -3,6 +3,7 @@ import type { Request, Response } from "express";
 import { body, validationResult} from "express-validator"
 
 import * as MemberService from "./member.service"
+import { Prisma } from "@prisma/client";
 
 export const memberRouter = express.Router();
 
@@ -16,20 +17,30 @@ memberRouter.get("/", async (request:Request, response:Response) =>{
             if(name && member){
                 return member.name===name?
                 response.status(200).json(member)
-                :response.status(204).json("No correlation")
+                :response.status(204).json("Não há correlação")
             }
-            return response.status(200).json(member);
+            return member?
+            response.status(200).json(member)
+            :response.status(404).json("Email não cadastrado");
         }
         else if(name){
             const members = await MemberService.listMembersByName(name as string);
-            return response.status(200).json(members);
+            return members?
+            response.status(200).json(members)
+            :response.status(404).json(`Nenhum usuário de nome ${name} não cadastrada`);
         }
 
         const members = await MemberService.listMembers();
         return response.status(200).json(members);
         
     } catch(error: any) {
-        return response.status(500).json(error.message);
+        if(error instanceof Prisma.PrismaClientKnownRequestError){
+            if(error.code === "P1011"){
+                return response.status(401).json(`Usuário teve acesso negado para o serviço`)
+            }
+        }
+        return response.status(500).json(`uepa${error.code} 
+             ${error.message}`);
     }
 })
 
@@ -37,7 +48,9 @@ memberRouter.get("/", async (request:Request, response:Response) =>{
 memberRouter.get("/:id", async (request:Request, response:Response) =>{
     try{
         const member = await MemberService.getMemberById(parseInt(request.params.id, 10))
-        return response.status(200).json(member);
+        return member?
+        response.status(200).json(member)
+        :response.status(404).json("Membro não encontrado");
     } catch (error:any){
         return response.status(500).json(error.message);
     }
@@ -46,24 +59,54 @@ memberRouter.get("/:id", async (request:Request, response:Response) =>{
 // POST: criar novo cadastro de membro
 
 memberRouter.post("/",
-    body("name").isString(),
-    body("email").isString(), 
-    body("password").isString(),
-    body("birth").isISO8601(), 
-    body("state").isString(),
-    body("city").isString(),
+    body("name").isString().withMessage("Name must be a string"),
+    body("email").isEmail().withMessage("Email must be a valid email address"),
+    body("password").isString().withMessage("Password must be a string"),
+    body("birth").isISO8601().withMessage("Birth date must be in ISO8601 format"),
+    body("state").isString().withMessage("State must be a string"),
+    body("city").isString().withMessage("City must be a string"),
     async (request: Request, response: Response) => {
+
         const errors = validationResult(request);
-        if(!errors.isEmpty()) {
+        if (!errors.isEmpty()) {
+            const customErrors = errors.array().map(err => {
+                if("path" in err){
+                switch (err.path) {
+                    case "name":
+                        return { param: err.path, msg: "Nome inválido: nome é um campo obrigatório e deve ser um string válido" };
+                    case "email":
+                        return { param: err.path, msg: "Email inválido: email é um campo obrigatório e deve ser um endereço de email válido" };
+                    case "birth":
+                        return { param: err.path, msg: "Data de nascimento inválida: data de nascimento é um campo obrigatório e deve estar no formato ISO8601" };
+                    case "state":
+                        return { param: err.path, msg: "Estado inválido: estado é um campo obrigatório e deve ser um string válido" };
+                    case "city":
+                        return { param: err.path, msg: "Cidade inválida: cidade é um campo obrigatório e deve ser um string válido" };
+                    default:
+                        return { param: err.path, msg: `${err.path} inválido: ${err.path} é um campo obrigatório e deve ser válido` };
+                }}
+                return err
+            });
+
             return response.status(400).json({
-                errors:errors.array()
+                errors: customErrors
             });
         }
+
+
         try{
             const member = request.body;
             const newMember = await MemberService.createMember(member);
             return response.status(201).json(newMember)
         } catch(error:any){
+            if(error instanceof Prisma.PrismaClientKnownRequestError){
+                if (error.code === "P1011"){
+                    return response.status(401).json(`Usuário teve acesso negado para o serviço`)
+                } else if (error.code === "P2002"){
+                    return response.status(409).json(`Endereço de email já cadastrado`)
+                }
+                return response.status(500).json(`${error.code}: ${error.message}`)
+            }
             return response.status(500).json(error.message);
         }
     }
@@ -72,25 +115,57 @@ memberRouter.post("/",
 // PUT: atualizar usuário por id
 
 memberRouter.put("/:id",
-    body("name").isString(),
-    body("email").isString(), 
-    body("password").isString(),
-    body("birth").isISO8601(), 
-    body("state").isString(),
-    body("city").isString(),
+    body("name").isString().withMessage("Name must be a string"),
+    body("email").isEmail().withMessage("Email must be a valid email address"),
+    body("password").isString().withMessage("Password must be a string"),
+    body("birth").isISO8601().withMessage("Birth date must be in ISO8601 format"),
+    body("state").isString().withMessage("State must be a string"),
+    body("city").isString().withMessage("City must be a string"),
     async (request: Request, response: Response) => {
+
         const errors = validationResult(request);
-        if(!errors.isEmpty()) {
+        if (!errors.isEmpty()) {
+            const customErrors = errors.array().map(err => {
+                if("path" in err){
+                switch (err.path) {
+                    case "name":
+                        return { param: err.path, msg: "Nome inválido: nome é um campo obrigatório e deve ser um string válido" };
+                    case "email":
+                        return { param: err.path, msg: "Email inválido: email é um campo obrigatório e deve ser um endereço de email válido" };
+                    case "birth":
+                        return { param: err.path, msg: "Data de nascimento inválida: data de nascimento é um campo obrigatório e deve estar no formato ISO8601" };
+                    case "state":
+                        return { param: err.path, msg: "Estado inválido: estado é um campo obrigatório e deve ser um string válido" };
+                    case "city":
+                        return { param: err.path, msg: "Cidade inválida: cidade é um campo obrigatório e deve ser um string válido" };
+                    default:
+                        return { param: err.path, msg: `${err.path} inválido: ${err.path} é um campo obrigatório e deve ser válido` };
+                }}
+                return err
+            });
+
             return response.status(400).json({
-                errors:errors.array()
+                errors: customErrors
             });
         }
+
+        
         const id: number = parseInt(request.params.id, 10)
-        try {
+        try{
             const member = request.body;
             const updatedMember = await MemberService.updateMember(member, id)
             return response.status(200).json(updatedMember)
         } catch (error:any) {
+            if(error instanceof Prisma.PrismaClientKnownRequestError){
+                if (error.code === "P1011"){
+                    return response.status(401).json(`Usuário teve acesso negado para o serviço`)
+                } else if (error.code === "P2002"){
+                    return response.status(409).json(`Endereço de email já cadastrado`)
+                } else if (error.code === "P2025"){
+                    return response.status(404).json("Usuário não encontrado")
+                }
+                return response.status(500).json(`${error.code}: ${error.message}`)
+            }
             return response.status(500).json(error.message);
         }
     }
