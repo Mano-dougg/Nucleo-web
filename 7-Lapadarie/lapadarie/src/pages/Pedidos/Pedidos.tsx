5ef068360c0c0dda908cf870193170e63c4936c7
Priscila Anjos <priscilaanjos@ufba.br>
2024-06-10 18:39:46 -0300

    feat:responsividade no que já tinha sido feito

--- 7-Lapadarie/lapadarie/src/pages/Pedidos/Pedidos.tsx
+++ 7-Lapadarie/lapadarie/src/pages/Pedidos/Pedidos.tsx
@@ -3,7 +3,20 @@ import Image from 'next/image';
 
 export default function Pedidos() {
     return (
-      <main className="pedidos">
+      
+      <main className="allpedidos">
+
+        <section className="pedidos">
+
+          <div className="addpedidos">
+            <button>+ Adicionar pessoa a fila</button>
+          </div>
+
+          <div className="pedidoslista">
+
+          </div>
+          
+        </section>
 
       </main>
     );
