3a376cc883dd8827a3aca7b43ae31f51c3ccda98
Mikelly Correia <163700231+MiCorreia@users.noreply.github.com>
2024-06-09 23:27:11 -0300

    Construindo a rota de criação do cliente

--- 7-Lapadarie (Em Dupla)/lapadarie-Back/src/server.ts
+++ 7-Lapadarie (Em Dupla)/lapadarie-Back/src/server.ts
@@ -1,12 +1,30 @@
 import express, { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 const app = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+app.use(express.json());
+
+//criando o usuário para pôr na fila
+app.post('/user', async (req: Request, res: Response) => {
+  const { nome, totalPao, totalPagar} = req.body;
+  const user = await prisma.user.create({
+    data: {
+      nome,
+      totalPao,
+      totalPagar
+    }
+    
+});
+
+res.status(201).json({ //padrão para mensagem de sucesso
+    message: 'Usuário adicionado a fila com sucesso!'});
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Servidor rodando na porta ${port}`)
 })
