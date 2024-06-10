3c23aae3279b39a1b45dbaa3b677add9228a75ab
Mikelly Correia <163700231+MiCorreia@users.noreply.github.com>
2024-06-10 18:09:41 -0300

    Pensando nos estados após clicar no botão para adicionar mais pessoas na fila

--- 7-Lapadarie (Em Dupla)/lapadarie-front/components/fila/fila.tsx
+++ 7-Lapadarie (Em Dupla)/lapadarie-front/components/fila/fila.tsx
@@ -1,13 +1,55 @@
+"use client";
+
 import Image from "next/image";
 import Lixeira from "../../public/lixeira.svg";
 import style from "./fila.module.css";
+import { use, useState } from "react";
 
 
-export default function Fila(){
+export default function Fila(){ 
+
+    const [mostrarForms, setMostrarForms] = useState(false);
+    const [nomeCliente, setnomeCliente] = useState<string>('');
+    const [totalPao, setTotalPao] = useState<number>(0);
+    const [totalPagar, setTotalPagar] = useState<number>(0);
+    const [fila, setFila] = useState<{ nome: string, pao: number, pagar: number }[]>([]);
+
+
+    const visivelAoClick = () =>{ //controlar a visibilidade do forms
+        setMostrarForms(!mostrarForms);
+    };
+
+    const enviarForms = (event: React.FormEvent) =>{ //controlar o envio do forms
+        event.preventDefault();
+        setFila([...fila, { nome: nomeCliente, pao: totalPao, pagar: totalPagar } ]);
+        setnomeCliente('');
+        setTotalPao(0);
+        setTotalPagar(0);
+        setMostrarForms(false);
+    }
+
     return(
         <div className={style.fila}>
 
-            <button className={style.botao}><h3>+ Adicionar pessoas a fila</h3></button>
+            <button className={style.botao}>
+                <h3>+ Adicionar pessoas a fila</h3>
+            </button>
+
+            <div className="formulario">
+                <h3>Adicionar pessoa a fila</h3>
+
+                <div className="campoForms">
+                    <label>Nome</label>
+                    <input 
+                            type="text" 
+                            value={nomeCliente}
+                            placeholder="Nome completo do cliente"
+                        />
+                </div>
+
+
+
+            </div>
 
             <div className={style.cards}>
                 <div className="info">
