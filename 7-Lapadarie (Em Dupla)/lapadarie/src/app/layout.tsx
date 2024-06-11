b36c476a744f7214ce33625eed7aa152c639de9d
Allan Barros Cruz <alfabeto.allanbc@gmail.comm>
2024-06-11 18:00:38 -0300

    add client menu added + "footer" creation

--- 7-Lapadarie (Em Dupla)/lapadarie/src/app/layout.tsx
+++ 7-Lapadarie (Em Dupla)/lapadarie/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins( {weight: ["100","200","300","400","500","600","700","800","900"], subsets: ["latin"] });
+const poppins = Poppins( {weight: ["100","200","300","400","500","600","700","800","900"], subsets: ["latin"], variable: "--font-poppins" });
 
 export const metadata: Metadata = {
   title: "Lapadarie",
