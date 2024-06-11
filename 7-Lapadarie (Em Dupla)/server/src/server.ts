8b31320cbeec7d3e945215ae85d140d85d6a6ea6
talitaester <talita.alves@ufba.br>
2024-06-11 11:44:58 -0300

    deleted:    7-Lapadarie (Em Dupla)/.gitignore 	new file:   7-Lapadarie (Em Dupla)/client/.eslintrc.json 	new file:   7-Lapadarie (Em Dupla)/client/.gitignore 	new file:   7-Lapadarie (Em Dupla)/client/README.md 	new file:   7-Lapadarie (Em Dupla)/client/app/favicon.ico 	new file:   7-Lapadarie (Em Dupla)/client/app/globals.css 	new file:   7-Lapadarie (Em Dupla)/client/app/layout.tsx 	new file:   7-Lapadarie (Em Dupla)/client/app/page.module.css 	new file:   7-Lapadarie (Em Dupla)/client/app/page.tsx 	new file:   7-Lapadarie (Em Dupla)/client/next.config.mjs 	new file:   7-Lapadarie (Em Dupla)/client/package.json 	new file:   7-Lapadarie (Em Dupla)/client/public/next.svg 	new file:   7-Lapadarie (Em Dupla)/client/public/vercel.svg 	new file:   7-Lapadarie (Em Dupla)/client/tsconfig.json 	deleted:    7-Lapadarie (Em Dupla)/prisma/schema.prisma 	new file:   7-Lapadarie (Em Dupla)/server/.env 	new file:   7-Lapadarie (Em Dupla)/server/.gitignore 	new file:   7-Lapadarie (Em Dupla)/server/package-lock.json 	new file:   7-Lapadarie (Em Dupla)/server/package.json 	new file:   7-Lapadarie (Em Dupla)/server/prisma/schema.prisma 	new file:   7-Lapadarie (Em Dupla)/server/src/server.ts 	renamed:    7-Lapadarie (Em Dupla)/tsconfig.json -> 7-Lapadarie (Em Dupla)/server/tsconfig.json

---
+++ 7-Lapadarie (Em Dupla)/server/src/server.ts
@@ -0,0 +1,15 @@
+import { configDotenv } from "dotenv";
+import { Request, Response } from "express";
+
+configDotenv();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
