1bf2afd76eaf482b6d6e370bb5d16fa812ba9fb9
Mikelly Correia <163700231+MiCorreia@users.noreply.github.com>
2024-06-11 20:13:19 -0300

    Forms

---
+++ 7-Lapadarie (Em Dupla)/lapadarie-front/components/forms/forms.tsx
@@ -0,0 +1,12 @@
+export default function Form (){
+    return(
+        <form>
+            
+            <div className="nome">
+                <input type="text" placeholder="Nome completo do cliente"></input>
+            </div>
+
+
+        </form>
+    )
+}
