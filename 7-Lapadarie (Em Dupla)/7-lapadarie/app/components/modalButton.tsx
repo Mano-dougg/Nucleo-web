636ad1cfd65d45eacd977de916e9aff32152749b
palmsb <paloma.brito020@gmail.com>
2024-06-12 18:52:26 -0300

    feat: corrigind input, inserindo form para interação com back, ajustando estilização

--- 7-Lapadarie (Em Dupla)/7-lapadarie/app/components/modalButton.tsx
+++ 7-Lapadarie (Em Dupla)/7-lapadarie/app/components/modalButton.tsx
@@ -4,9 +4,21 @@
 import { useState } from "react"
 import Modal from "./modal"
 import './modal.css'
+/*import { GetServerSideProps } from "next"
+import {prisma}
+
+*/
+interface FormData{
+    nome: string,
+    paes: string,
+}
 
 export default function ModalButton(){
     const [openModal, setOpenModal] = useState(false)
+    const [form, setForm] = useState<FormData>({nome: '', paes: ''})
+
+    /*async function create(data:FormData*/
+    /*const handleSubmit = async (data:...*/
 
     return(
     <main>
@@ -16,12 +28,36 @@ export default function ModalButton(){
             </button>
         </div>
 
+        {/* mostrar as tabelas */}
+        {/* <div className="tabelas">
+            <u>
+            {exemplos.map(exemplo => (
+              <li key={exemplo.id} className="listaCliente">
+                <div>
+                    <h3>{exemplo.nome}</h3>
+                </div>
+                <div>
+                    <p>Total de pães: {exemplo.paes}</p>
+                    <p>Total a pagar: </p>
+                </div>
+              </li> 
+            ))}
+            </u>
+        </div> */}
+
         <div>
           <Modal isOpen={openModal} setModalOpen={()=> setOpenModal(!openModal)}>
             <div>
                 <p>Adicionar pessoa a fila</p>
-                <input type="text" value="" placeholder='Nome Completo do Cliente'/>
-                <input type="text" value="" placeholder='Total de Pães:' />
+                <form onSubmit={e => {
+                    e.preventDefault()
+                    /*handleSubmit*/
+                }}>
+                    <input className="inputData" type="text" /*value={form.nome}*/ placeholder='Nome Completo do Cliente'/* onChange={e=> setForm({...form, nome: e.target.value})}*//>
+                    <input className="inputData" type="number" /*value={form.paes}*/ placeholder='Total de Pães:' /*onChange={e => setForm({...form, paes: e.target.value})} *//>
+                    <button type="submit" className='btnModalMarrom'>Enviar</button>
+                </form>
+
             </div>
 
           </Modal>
@@ -29,3 +65,13 @@ export default function ModalButton(){
     </main>
     )
 }
+
+/*export const getServerSideProps: GetServerSideProps = async () => {
+    select:{
+    nome: true
+    paes: true
+    }
+    return{
+    props: {
+    }}
+}*/
