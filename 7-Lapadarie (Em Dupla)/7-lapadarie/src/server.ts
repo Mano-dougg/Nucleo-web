5c574928ca62232cfa2b136b4073b73a298234af
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-06-12 18:54:29 -0300

    feat: Instalados prisma, expressJS, etc. Feita função de post

---
+++ 7-Lapadarie (Em Dupla)/7-lapadarie/src/server.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client/extension';
+import { configDotenv } from 'dotenv';
+import express, { Request, Response } from 'express';
+
+configDotenv()
+const prisma = new PrismaClient()
+const app = express();
+const PORT = 3001;
+
+app.get("/helloworld", (req: Request, res: Response) => {
+  res.send("｢ʜȷxʃ ɵȷ xʃɤʃɞʃʭ｣ Esta não é a página de front-end. ;P")
+})
+
+app.post("/",
+  async (req: Request, res: Response) => {
+    const {nome, quantidade} = req.body
+    const pedido = await prisma.pedido.create({
+      data: {
+        cliente: nome,
+        quant: quantidade
+      }
+    })
+  }
+)
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+})
