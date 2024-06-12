12f6d9cfadb894e196a7c2c46dc5ede7489bf042
Elis-Vasconcelos <167431790+Elis-Vasconcelos@users.noreply.github.com>
2024-06-12 02:37:40 -0300

    Front mostrando clientes presentes na fila

--- 7-Lapadarie (Em Dupla)/lapadarie-front/components/fila/fila.tsx
+++ 7-Lapadarie (Em Dupla)/lapadarie-front/components/fila/fila.tsx
@@ -1,39 +1,70 @@
-import React, { useState } from 'react';
-import style from './fila.module.css';
+import style from "./fila.module.css";
+import { useEffect, useState } from "react";
+import Modal from '../modal/modal'
+import axios from "axios";
+interface Cliente {
+    id: number;
+    nome: string;
+    totalPao: number;
+    totalPagar: number;
+  }
 
-export default function Form() {
-    const [nomeCliente, setNomeCliente] = useState('');
-    const [totalPao, setTotalPao] = useState(0);
+export default function Fila(){ 
 
-    // Função para calcular o preço
-    const calcularPreco = () => {
-        const preco = totalPao * 2;
-        return preco;
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const [clientes, setClientes] = useState<Cliente[]>([]);
+
+    const fetchClientes = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/clientes');
+        setClientes(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar clientes:', error);
+      }
     };
 
-    return (
-        <form className={style.formulario}>
-            <div className="nome">
-                <input
-                    type="text"
-                    placeholder="Nome completo do cliente"
-                    value={nomeCliente}
-                    onChange={(e) => setNomeCliente(e.target.value)}
-                />
-            </div>
+    useEffect(() => {
+        // Busca dados iniciais
+        fetchClientes();
 
-            <div className="pao">
-                <input
-                    type="number"
-                    placeholder="Total de pão"
-                    value={totalPao}
-                    onChange={(e) => setTotalPao(Number(e.target.value))}
-                />
-            </div>
+        // Configura intervalo para buscar dados
+        const interval = setInterval(fetchClientes, 1000); // Busca a cada 1 segundos
 
-            <div>
-                Preço: R$ {calcularPreco()}
+        // Limpa o intervalo quando o componente é desmontado
+        return () => clearInterval(interval);
+      }, []);
+
+    return(
+        <div className={style.fila}>
+
+            <button className={style.botao} onClick={() => setModalOpen(true)}>
+                <h3> + Adicionar pessoas a fila</h3>
+            </button>
+
+            <div className="modal">
+
+                <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}/>
+
             </div>
-        </form>
-    );
+            
+            {clientes.map(cliente => (
+                <div key={cliente.id} className={style.cards}>
+                <div className="info">
+                    <h4>{cliente.nome}</h4>
+                    <div className={style.compra}>
+                    <h6>Total de pão: {cliente.totalPao}</h6>
+                    <h6>Total a pagar: {cliente.totalPagar}</h6>
+                    </div>
+                </div>
+
+                <button className={style.lixo}>
+                    <img className='lixo' src={"/lixeira.svg"} alt="lixeira" />
+                </button>
+
+
+            </div>
+            ))}
+        </div>
+    )
 }
