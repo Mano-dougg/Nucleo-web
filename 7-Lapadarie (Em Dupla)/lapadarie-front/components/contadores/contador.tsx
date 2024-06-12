3fee46114dddddc8a5bc1b03e655ca21a493e74f
Mikelly Correia <163700231+MiCorreia@users.noreply.github.com>
2024-06-11 22:59:02 -0300

    fetch para atualizar a página após o contador rodar - live share

--- 7-Lapadarie (Em Dupla)/lapadarie-front/components/contadores/contador.tsx
+++ 7-Lapadarie (Em Dupla)/lapadarie-front/components/contadores/contador.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Pessoa from "../../public/iconPessoas.svg";
-import Carrinho from"../../public/iconCarrinho.svg";
+import Carrinho from "../../public/iconCarrinho.svg";
 import Dinheiro from "../../public/iconDinheiro.svg";
 import style from "./contador.module.css";
 import { useEffect, useState } from "react";
@@ -8,21 +8,30 @@ import axios from "axios";
 
 const apiUrl = 'http://localhost:3000/getEstatisticas';
 
-export default function Contador(){
+export default function Contador() {
     const [estatisticas, setEstatisticas] = useState({ totalPao: 0, totalPagar: 0, tamanhoFila: 0 });
 
-    useEffect(() => {
-        // Fetch dados das estatisticas
-        axios.get(apiUrl)
-          .then(response => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(apiUrl);
             setEstatisticas(response.data);
-          })
-          .catch(error => {
+        } catch (error) {
             console.error('Error fetching dados das estatisticas:', error);
-          });
-      }, []);
+        }
+    };
 
-    return(
+    useEffect(() => {
+        // Busca dados iniciais
+        fetchData();
+
+        // Configura intervalo para buscar dados
+        const interval = setInterval(fetchData, 1000); // Busca a cada 1 segundos
+
+        // Limpa o intervalo quando o componente é desmontado
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
         <div className={style.contadores}>
 
             <div className={style.fila}>
@@ -35,7 +44,6 @@ export default function Contador(){
 
             </div>
 
-
             <div className={style.pao}>
                 <div className={style.topo}>
                     <h6>Pães vendidos</h6>
@@ -45,7 +53,6 @@ export default function Contador(){
                 <h3>{estatisticas.totalPao}</h3> {/* A quantidade de pães vendidos tem que ser um dado que vem do banco de dados*/}
 
             </div>
-            
 
             <div className={style.totalVenda}>
                 <div className={style.topo1}>
@@ -56,7 +63,6 @@ export default function Contador(){
                 <h3>R$ {estatisticas.totalPagar.toFixed(2)}</h3> {/* O valor total de pão vendido tem que ser um dado que vem do banco de dados*/}
 
             </div>
-
         </div>
-    )
+    );
 }
