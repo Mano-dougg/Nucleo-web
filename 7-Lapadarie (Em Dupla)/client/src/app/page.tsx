0ee1f81bb091de65801948a78e21d7331245e648
talitaester <talita.alves@ufba.br>
2024-06-12 23:50:15 -0300

    deleted:    7-Lapadarie (Em Dupla)/client/.eslintrc.cjs 	new file:   7-Lapadarie (Em Dupla)/client/.eslintrc.json 	modified:   7-Lapadarie (Em Dupla)/client/.gitignore 	modified:   7-Lapadarie (Em Dupla)/client/README.md 	deleted:    7-Lapadarie (Em Dupla)/client/index.html 	new file:   7-Lapadarie (Em Dupla)/client/lib/axios.ts 	new file:   7-Lapadarie (Em Dupla)/client/next.config.mjs 	modified:   7-Lapadarie (Em Dupla)/client/package-lock.json 	modified:   7-Lapadarie (Em Dupla)/client/package.json 	deleted:    7-Lapadarie (Em Dupla)/client/public/vite.svg 	deleted:    7-Lapadarie (Em Dupla)/client/src/App.css 	deleted:    7-Lapadarie (Em Dupla)/client/src/App.tsx 	new file:   7-Lapadarie (Em Dupla)/client/src/app/assets/Cart-icon.png 	new file:   7-Lapadarie (Em Dupla)/client/src/app/assets/Delete-icon.png 	new file:   7-Lapadarie (Em Dupla)/client/src/app/assets/Dolar-icon.png 	new file:   7-Lapadarie (Em Dupla)/client/src/app/assets/Logo.png 	new file:   7-Lapadarie (Em Dupla)/client/src/app/assets/Queue-icon.png 	new file:   7-Lapadarie (Em Dupla)/client/src/app/components/Footer/Footer.module.css 	new file:   7-Lapadarie (Em Dupla)/client/src/app/components/Footer/Footer.tsx 	new file:   7-Lapadarie (Em Dupla)/client/src/app/components/Forms/CreateOrder.css 	new file:   7-Lapadarie (Em Dupla)/client/src/app/components/Forms/CreateOrder.tsx 	new file:   7-Lapadarie (Em Dupla)/client/src/app/components/Header/Header.css 	new file:   7-Lapadarie (Em Dupla)/client/src/app/components/Header/Header.tsx 	new file:   7-Lapadarie (Em Dupla)/client/src/app/components/History/History.tsx 	new file:   7-Lapadarie (Em Dupla)/client/src/app/components/Queue/Queue.css 	new file:   7-Lapadarie (Em Dupla)/client/src/app/components/Queue/Queue.tsx 	new file:   7-Lapadarie (Em Dupla)/client/src/app/globals.css 	new file:   7-Lapadarie (Em Dupla)/client/src/app/layout.tsx 	new file:   7-Lapadarie (Em Dupla)/client/src/app/page.css 	new file:   7-Lapadarie (Em Dupla)/client/src/app/page.tsx 	new file:   7-Lapadarie (Em Dupla)/client/src/app/ui/fonts.ts 	deleted:    7-Lapadarie (Em Dupla)/client/src/assets/logo.png 	deleted:    7-Lapadarie (Em Dupla)/client/src/index.css 	deleted:    7-Lapadarie (Em Dupla)/client/src/main.tsx 	deleted:    7-Lapadarie (Em Dupla)/client/src/vite-env.d.ts 	modified:   7-Lapadarie (Em Dupla)/client/tsconfig.json 	deleted:    7-Lapadarie (Em Dupla)/client/tsconfig.node.json 	deleted:    7-Lapadarie (Em Dupla)/client/vite.config.ts 	modified:   7-Lapadarie (Em Dupla)/server/prisma/dev.db

---
+++ 7-Lapadarie (Em Dupla)/client/src/app/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+import "./page.css";
+import axios from 'axios';
+
+import Header from "./components/Header/Header";
+import Queue from "./components/Queue/Queue";
+import History from "./components/History/History";
+import CreateOrder from "./components/Forms/CreateOrder";
+
+import React, { useEffect, useState } from "react";
+
+interface Order {
+  id: number;
+  customer_name: string;
+  numberOfBreads: number;
+  totalPrice: number;
+  createdAt: string;
+  updatedAt: string;
+  FulfilledOrder: boolean;
+}
+
+
+export default function Home() {
+
+  // State para ver se o form de adicionar cliente/pedido está aberto
+  const [createOrder, setCreateOrder] = useState<boolean>(false);
+
+  // adiciona classe ao html para não haver overflow-y (para que o form ocupe toda tela)
+  useEffect(() => {
+    if (createOrder) {
+      document.documentElement.classList.add('no-scroll');
+    } else {
+      document.documentElement.classList.remove('no-scroll');
+    }
+  }, [createOrder]);
+
+
+  // abrir form de adicionar cliente/pedido
+  const OpenCreateOrderForm = () => {
+    setCreateOrder(true);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  // fechar form de adicionar cliente/pedido
+  const CloseCreateOrderForm = () =>{
+    setCreateOrder(false);
+  }
+
+
+  return (
+    <div className="home">
+      
+      <Header></Header>
+
+      {createOrder?
+      <CreateOrder closeCreateOrderForm={CloseCreateOrderForm} ></CreateOrder>: ""}
+
+      <main className="main">
+
+        <button className="add-order" onClick={OpenCreateOrderForm}> + Adicione pessoas a fila </button>
+
+        <Queue></Queue>
+
+        <p className="finished"> Finalizados </p>
+
+        <History></History>
+
+      </main>
+
+    </div>
+  );
+}
