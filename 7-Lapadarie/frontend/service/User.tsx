1178e081ba4afcc3d8ac1e48d1c078915cdf77ae
Priscila Anjos <priscilaanjos@ufba.br>
2024-06-13 20:57:48 -0300

    lapadarie

---
+++ 7-Lapadarie/frontend/service/User.tsx
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+export const axiosInstance = axios.create({
+    baseURL: "http://localhost:3001"
+
+})
+
+export class User{
+
+    listarall(){
+        return axiosInstance.get('/usuarios')
+    }
+}
+
+export class Totais{
+
+    totasss(){
+        return axiosInstance.get('/totais')
+    }
+}
+
+export class Postar{
+
+    Criar(){
+        return axiosInstance.post('/')
+    }
+    
+}
+
+export class Atualizar{
+
+    Atualizar(id: any, data: any) {
+        return axiosInstance.put(`/usuarios/${id}`, data);
+    }
+    
+}
--- 7-Lapadarie/frontend/src/app/page.tsx
+++ 7-Lapadarie/frontend/src/app/page.tsx
@@ -1,8 +1,11 @@
+'use client'
 import Header from "@/pages/Header/Header";
 import Informacoes from "@/pages/Informacoes/informacoes";
 import styles from "./page.module.css";
 import Pedidos from "@/pages/Pedidos/Pedidos";
 import Footer from "@/pages/Footer/Footer";
+import { User } from "../../service/User";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
