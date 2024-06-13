e8e9b38b1d7436a0475f6a0182fe7004db5dedbb
David <151107969+EnderZerobl@users.noreply.github.com>
2024-06-13 08:18:15 -0300

    Feat:Delete button

--- 7-Lapadarie (Em Dupla)/lapadariet/src/pages/Pessoas/pessoas.tsx
+++ 7-Lapadarie (Em Dupla)/lapadariet/src/pages/Pessoas/pessoas.tsx
@@ -1,47 +1,54 @@
-'use client'
-import Header from "@/pages/Header/Header";
-import Informacoes from "@/pages/Informacoes/informacoes";
-import styles from "./page.module.css";
-import Pedidos from "@/pages/Pedidos/Pedidos";
-import Footer from "@/pages/Footer/Footer";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { User } from "../../../service/User";
-import axios from "axios";
 
-interface Dado {
+interface Da {
   id: number;
   nome: string;
   paes: number;
 }
 
 export default function Pessoas() {
-    const [data, setData] = useState<Dado[] | null>(null);
-    const UserA = new User();
-  
-    useEffect(() => {
-      UserA.listarall()
-      .then((response)=>{
-        console.log(response.data)
-        setData(response.data);
-      }).catch((error)=>{
-        console.log(error)
-      })
-    }, []);
+  const [data, setData] = useState<Da[] | null>(null);
+  const UserA = new User();
 
-    
-    return (
-        <div>
-        {data ? (
-          <div>
-            <h1>Clientes</h1>
-            {data.map((item) => (
-              <p key={item.id}>{item.nome}: {item.paes} pães</p>
-            ))}
-          </div>
-        ) : (
-          <p>Carregando dados...</p>
-        )}
-      </div>
-    );
+  useEffect(() => {
+    UserA.listarall()
+    .then((response)=>{
+      console.log(response.data)
+      setData(response.data);
+    }).catch((error)=>{
+      console.log(error)
+    })
+  }, []);
+
+  const Apagar = async (id: number) => {
+    try {
+    if (data){
+      await axios.delete(`http://localhost:3001/${id}`);
+      setData(data.filter(item => item.id !== id));
+    }
+    } catch (error) {
+      console.error('Erro', error);
+    }
   }
+
+  return (
+    <div>
+      {data ? (
+        <div>
+          <h1>Clientes</h1>
+          {data.map((item) => (
+            <div key={item.id}>
+              <p>{item.nome}: {item.paes} pães</p>
+              <button onClick={() => Apagar(item.id)}>Deletar</button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Carregando dados...</p>
+      )}
+    </div>
+  );
+}
   
