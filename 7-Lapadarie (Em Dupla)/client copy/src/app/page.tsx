c821c4096cf3f1d2c5f3aa56b39efe1ea1304c1d
Beatriz Pereira Espinheira <beatrizespinheiraa@gmail.com>
2024-06-14 12:52:58 -0300

    feat: adiciona opção de finalizar pedido e componente para edição

--- 7-Lapadarie (Em Dupla)/client copy/src/app/page.tsx
+++ 7-Lapadarie (Em Dupla)/client copy/src/app/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 import "./page.css";
+import axios from 'axios';
 
 import Header from "./components/Header/Header";
 import Queue from "./components/Queue/Queue";
 import History from "./components/History/History";
 import CreateOrder from "./components/Forms/CreateOrder";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+
+interface Order {
+  id: number;
+  customer_name: string;
+  numberOfBreads: number;
+  totalPrice: number;
+  createdAt: string;
+  updatedAt: string;
+  FulfilledOrder: boolean;
+}
 
 
 export default function Home() {
@@ -27,14 +38,12 @@ export default function Home() {
   // abrir form de adicionar cliente/pedido
   const OpenCreateOrderForm = () => {
     setCreateOrder(true);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   // fechar form de adicionar cliente/pedido
   const CloseCreateOrderForm = () =>{
     setCreateOrder(false);
   }
-
 
   return (
     <div className="home">
