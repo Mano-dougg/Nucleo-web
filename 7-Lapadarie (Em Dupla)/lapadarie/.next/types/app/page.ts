229f0a2fb63240a76200db8bd4f8170e2093d563
Allan Barros Cruz <alfabeto.allanbc@gmail.comm>
2024-06-15 18:38:05 -0300

    modify order button + more resposiveness shenanigans

--- 7-Lapadarie (Em Dupla)/lapadarie/.next/types/app/page.ts
+++ 7-Lapadarie (Em Dupla)/lapadarie/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\bruno\Desktop\repositorios\InfoJR\nucleo-web\7-Lapadarie (Em Dupla)\lapadarie\src\app\page.tsx
+// File: C:\Users\allan\nucleo-web\nucleo-web\7-Lapadarie (Em Dupla)\lapadarie\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
