186bce8f50d83c458c747c929cc600901c7c4e20
palmsb <paloma.brito020@gmail.com>
2024-06-14 21:45:16 -0300

    Feat: Implementando tabela de clientes

--- 7-Lapadarie (Em Dupla)/7-lapadarie/app/components/tabela.tsx
+++ 7-Lapadarie (Em Dupla)/7-lapadarie/app/components/tabela.tsx
@@ -1,28 +1,50 @@
 import { Pedido } from "@prisma/client";
 import { GetStaticProps } from "next";
 import { prisma } from "../../lib/prisma"
+import './modal.css'
+import { FaRegTrashAlt } from "react-icons/fa";
 
 interface PedidosProps{
     pedidos: Array<Pedido>;
 }
 
 export default function Tabela({pedidos}: PedidosProps){
+    
     return(
-        <div>
-            {pedidos.map((pedido, index) => (
-                <div key ={index}>
-                    <span>{pedido.cliente}</span>
-                    <span>{pedido.quant}</span>
-                </div>
-            ))}
-        </div>
+        <div className="divTabelaOut">
+            <div className="divTabela">
+                    {pedidos.map((pedido, index) => (
+                    <div key ={index}>
+
+                        <span><h1>{pedido.cliente}</h1></span>
+
+                        <div className="divTabelaItens">
+                            <span><p>Total de Pães:{pedido.quant}</p></span>
+                            <p>Total a Pagar: R$</p>
+
+                        </div>
+
+                        <div>
+                            <button className="btnDelete"><FaRegTrashAlt size={25}/></button>
+                        </div>
+
+                    </div>
+                /*icone*/
+            
+
+                
+                    
+                ))}
+            </div>
+       </div>
     )
 }
 
 export const getStaticProps: GetStaticProps = async (context) =>{
 
     const pedidos = await prisma.pedido.findMany()
-
+    console.log(pedidos)
+    
     return{
         props: {
             pedidos,
@@ -31,3 +53,4 @@ export const getStaticProps: GetStaticProps = async (context) =>{
         revalidate: 10,
     };
 };
+
