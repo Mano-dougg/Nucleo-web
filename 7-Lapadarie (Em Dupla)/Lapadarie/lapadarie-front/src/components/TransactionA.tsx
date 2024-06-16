789cf808f9e096655f0d35077523b8e7e23850e9
Mauricio Matchal <mauriciomatchal@gmail.com>
2024-06-15 23:30:37 -0300

    feat: responsividade

--- 7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/components/TransactionA.tsx
+++ 7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/components/TransactionA.tsx
@@ -9,8 +9,11 @@ interface TransactionProps {
 
 const Transaction: React.FC<TransactionProps> = ({ amount }) => {
   return (
-    <div id="transactionQ" className="bg-pbrown max-w-[100%] h-[136px] flex flex-col justify-between p-[25px] pt-[20px] pb-[18px] rounded-md font-sans text-white">
-      <div className="text-base flex flex-row justify-between items-center">
+    <div
+      id="transactionQ"
+      className="bg-pbrown max-w-[100%] h-[136px] flex flex-col justify-between p-[25px] pt-[20px] pb-[18px] rounded-md font-sans text-white"
+    >
+      <div className="text-base font-sans flex flex-row justify-between items-center">
         <p>Entrada</p>
         <DollarSign />
       </div>
