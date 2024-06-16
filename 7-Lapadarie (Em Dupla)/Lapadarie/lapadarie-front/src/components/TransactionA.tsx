9c37b88fa8144d208753ac7d0d121b989f83e2dd
Mauricio Matchal <mauriciomatchal@gmail.com>
2024-06-15 22:44:19 -0300

    modified:   7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-back/prisma/dev.db 	modified:   7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/app/page.tsx 	modified:   7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/components/Background.tsx 	modified:   7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/components/Botao_Popup.tsx 	modified:   7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/components/TransactionA.tsx 	modified:   7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/components/TransactionB.tsx 	modified:   7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/components/TransactionQ.tsx

--- 7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/components/TransactionA.tsx
+++ 7-Lapadarie (Em Dupla)/Lapadarie/lapadarie-front/src/components/TransactionA.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { default as DollarSign } from "./icons/DollarSign";
+import "../app/globals.css";
 
 interface TransactionProps {
   // Define any props for your component here
@@ -8,7 +9,7 @@ interface TransactionProps {
 
 const Transaction: React.FC<TransactionProps> = ({ amount }) => {
   return (
-    <div className="bg-pbrown max-w-[100%] h-[136px] flex flex-col justify-between p-[25px] pt-[20px] pb-[18px] rounded-md font-sans text-white">
+    <div id="transactionQ" className="bg-pbrown max-w-[100%] h-[136px] flex flex-col justify-between p-[25px] pt-[20px] pb-[18px] rounded-md font-sans text-white">
       <div className="text-base flex flex-row justify-between items-center">
         <p>Entrada</p>
         <DollarSign />
