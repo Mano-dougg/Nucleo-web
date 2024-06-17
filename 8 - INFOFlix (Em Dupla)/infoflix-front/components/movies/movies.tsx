6be97cac58d37fd25b3f6000a53226d13534048e
Mikelly Correia <163700231+MiCorreia@users.noreply.github.com>
2024-06-17 13:51:42 -0300

    Ajeitando o tamanho das imagens

--- 8 - INFOFlix (Em Dupla)/infoflix-front/components/movies/movies.tsx
+++ 8 - INFOFlix (Em Dupla)/infoflix-front/components/movies/movies.tsx
@@ -1,41 +1,49 @@
 import React, { useEffect, useState } from "react";
-import styles from "./movies.module.css"
+import Image from "next/image";
+import styles from "./movies.module.css";
 
 interface Movie {
     poster_path: string; // Caminho para a imagem do pôster
     title: string;       // Título do filme
-
+    id: number;          // ID do filme (para usar como key)
 }
 
 export default function Movies() {
-    const [movieList, setMovieList] = useState<Movie[]>([])
+    const [movieList, setMovieList] = useState<Movie[]>([]);
     
-
     const getMovie = async () => {
         try {
             const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=eb4a0700966ecd9a61881d4b79da8fcb');
             const data = await response.json(); // Converte a resposta para JSON
             setMovieList(data.results); // Supondo que 'results' seja o array de filmes
-        } catch(error) {
+        } catch (error) {
             console.error("Error fetching movies", error);
         }
-    }
+    };
     
     useEffect(() => {
         getMovie();
     }, []);
 
-    console.log(movieList);
-
     return (
         <div className={styles.exibicao}>
-            {movieList.map((movie) => {
-                return (
-                    <div className={styles.filmes}>
-                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-                    </div>
-                )
-            })}
+            <h3>Lançamentos</h3>
+            <>
+            
+            {movieList.map((movie, index) => (
+                <div key={movie.id} className={styles.filmes}>
+                    <Image 
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                        alt={movie.title}
+                        width={500} 
+                        height={750} 
+                        layout="responsive"
+                        className={styles.image}
+                        priority={index === 0} // Adiciona prioridade à primeira imagem
+                    />
+                </div>
+            ))}
+            </>
         </div>
-    )
+    );
 }
--- 8 - INFOFlix (Em Dupla)/infoflix-front/next.config.mjs
+++ 8 - INFOFlix (Em Dupla)/infoflix-front/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['image.tmdb.org'],
+    },
+  };
+  
+  export default nextConfig;
+  
+  
