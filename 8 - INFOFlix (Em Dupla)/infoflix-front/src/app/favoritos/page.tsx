34efacef7c31c2aa6ec86dcc9c4b9909a2ff1d96
Mikelly Correia <163700231+MiCorreia@users.noreply.github.com>
2024-06-21 17:35:42 -0300

    Tentando passar favoritos para outra página

--- 8 - INFOFlix (Em Dupla)/infoflix-front/src/app/favoritos/page.tsx
+++ 8 - INFOFlix (Em Dupla)/infoflix-front/src/app/favoritos/page.tsx
@@ -1,10 +1,26 @@
+// src/pages/index.tsx
+import React, { useState } from 'react';
+import Login from '../../../components/login/Login';
+import Movies from '../../../components/movies/movies';
+import Favoritos from '../../../components/favoritos/favoritos';
+import { useRouter } from 'next/router';
 
-export default function Favoritos() {
-    return (
-        <>
-            <div className="titulo">
-                <h3>FILMES FAVORITOS</h3>
-            </div>
-        </>    
-    );
-}
+const App = () => {
+  const [userId, setUserId] = useState<number | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const router = useRouter();
+
+  if (!userId || !token) {
+    return <Login setUserId={setUserId} setToken={setToken} />;
+  }
+
+  return (
+    <div>
+      <button onClick={() => router.push('/movies')}>Movies</button>
+      <button onClick={() => router.push('/favoritos')}>Favoritos</button>
+      {/* You can add more navigation buttons or links here */}
+    </div>
+  );
+};
+
+export default App;
