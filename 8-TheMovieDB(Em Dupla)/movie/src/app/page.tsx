f925af5086025f2bf90fbdb33e848a763b0e4f27
Gabriel <gabriellealoliveira0@gmail.com>
2024-06-21 17:40:18 -0300

    Logo add

---
+++ 8-TheMovieDB(Em Dupla)/movie/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import styled from "styled-components";
+import React from "react";
+import Header from "../../components/Header";
+
+export default function Home() {
+  return (
+    <>
+  <Header/>
+    </>
+  );
+}
