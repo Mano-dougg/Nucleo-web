91c5950969bf6174dac157c088c6e6424ac778ee
Fernando Costa <costa.fernando@ufba.br>
2024-06-21 16:51:19 -0300

    feat: configures eslint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,6 @@ import app from './app';
 const PORT = process.env.EXPRESS_PORT || 3001;
 
 const server = app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Server is running on port ${PORT}`);
 });
 
