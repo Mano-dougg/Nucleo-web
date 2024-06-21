aa1822ec41356784b72a1b212440947694afe62f
Priscila Anjos <priscilaanjos@ufba.br>
2024-06-21 20:48:56 -0300

    feat: alterações css

---
+++ 8-TheMovieDB/frontend/src/app/Home/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { useRouter } from "next/navigation";
+import "./home.css";
+
+export default function Home() {
+
+    return (
+        <section className="login-container">
+            
+        </section>
+    );
+}
--- 8-TheMovieDB/frontend/src/app/Login/page.tsx
+++ 8-TheMovieDB/frontend/src/app/Login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import "./login.css";
 
 export default function Login() {
@@ -9,22 +10,28 @@ export default function Login() {
         router.push("/Cadastro");
     };
 
+    const handleHome = () => {
+        router.push("/Home");
+    };
+
     return (
         <section className="login-container">
+
+            <Image src="/cinema.png" alt="" width={100} height={100} />
             
             <form className="form-login">
 
                 <h1>Login</h1>
 
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" name="email" required />
+                <input type="email" id="email" name="email" className="email-icon" required />
 
                 <label htmlFor="password">Senha</label>
-                <input type="password" id="password" name="password" required />
+                <input type="password" id="password" name="password" className="senha-icon" required />
 
                 <div className="botoes-login">
-                    <button type="submit" className="entrar">Entrar</button>
-                    <button onClick={handleSignUpClick} className="botao-cadastro">Cadastre-se</button>
+                    <button onClick={handleHome} type="submit" className="entrar">Entrar</button>
+                    <p>Não tem uma conta? <button onClick={handleSignUpClick} className="botao-cadastro">Cadastre-se</button></p>
                 </div>
 
                 
