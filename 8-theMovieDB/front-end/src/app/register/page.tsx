bd40f30746f4839f8dbc99ed9119138ea291a77c
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-06-23 17:38:31 -0300

    feat: add the post method

--- 8-theMovieDB/front-end/src/app/register/page.tsx
+++ 8-theMovieDB/front-end/src/app/register/page.tsx
@@ -1,18 +1,69 @@
+"use client"
+import { useState } from 'react';
 import Logo from "@/components/Logo";
 import Link from "next/link";
+import axios from "axios";
 
 export default function Register() {
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [error, setError] = useState(null);
+
+    const registerSubmit = async (event:any) => {
+        event.preventDefault();
+        
+        try {
+            await axios.post("http://localhost:8080/auth/register", {
+                nome: nome,
+                email: email,
+                senha: senha,
+            });
+            setNome('');
+            setEmail('');
+            setSenha('');
+            setError(null);
+        } catch (error: any) {
+            setError(error.response?.data?.message || "An error occurred during registration.");
+        }
+    };
+
     return (
         <>
             <Link href="/"><Logo /></Link>
             
             <section className="register">
-                <form action="/register" method="post">
-                    <input type="text" name="nome" id="nome" placeholder="Usuário" required />
-                    <input type="email" name="email" id="email" placeholder="Email" required />
-                    <input type="password" name="senha" id="senha" placeholder="Senha" required />
+                <form onSubmit={registerSubmit}>
+                    <input 
+                        type="text" 
+                        name="nome" 
+                        id="nome" 
+                        placeholder="Nome" 
+                        value={nome} 
+                        onChange={(e) => setNome(e.target.value)} 
+                        required 
+                    />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        placeholder="Email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        required 
+                    />
+                    <input 
+                        type="password" 
+                        name="senha" 
+                        id="senha" 
+                        placeholder="Senha" 
+                        value={senha} 
+                        onChange={(e) => setSenha(e.target.value)} 
+                        required 
+                    />
                     <input type="submit" value="Cadastrar" />
                 </form>
+                {error && <p className="error">{error}</p>}
             </section>
         </>
     );
