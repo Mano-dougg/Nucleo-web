2429199ecb6249d3628da6d5596a8bfdec23008c
Priscila Anjos <priscilaanjos@ufba.br>
2024-06-23 16:53:08 -0300

    feat: header

--- 8-TheMovieDB/frontend/src/app/Header/page.tsx
+++ 8-TheMovieDB/frontend/src/app/Header/page.tsx
@@ -1,17 +1,40 @@
-'use client'
-import { useRouter } from "next/navigation";
+// page.tsx
+import React, { useState } from "react";
+import Image from "next/image";
 import "./header.css";
 
 export default function Header() {
+    const [hovered, setHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setHovered(false);
+    };
 
     return (
-        <header className="header-container">
-
-            <section className="logo-header">
-                <h1>POP<span>Movies</span></h1>
+        <header
+            className={`header-container ${hovered ? 'expanded' : ''}`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <section className="icons-header">
+                <div className="navbar">
+                    <button><Image src="/movie.png" alt="Exemplo" width={30} height={30} />
+                    {hovered && ( <p className="additional-content"> Ver filmes </p> )}</button> 
+                </div>
+                <div className="navbar">
+                    <button><Image src="/search.png" alt="Exemplo" width={30} height={30} />
+                    {hovered && ( <p className="additional-content"> Procurar filmes </p> )}</button> 
+                </div>
+                <div className="navbar">
+                    <button><Image src="/heart.png" alt="Exemplo" width={30} height={30} />
+                    {hovered && ( <p className="additional-content"> Meus favoritos </p> )}</button> 
+                </div>
             </section>
-            
-            
+
         </header>
     );
 }
