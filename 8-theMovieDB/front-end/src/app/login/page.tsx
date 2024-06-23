3962eb5268ada1c07f9e39dc6e53d97425a41e5d
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-06-23 18:14:21 -0300

    feat: add post method in login

--- 8-theMovieDB/front-end/src/app/login/page.tsx
+++ 8-theMovieDB/front-end/src/app/login/page.tsx
@@ -1,21 +1,59 @@
 'use client'
 
+import { useState } from 'react';
 import Logo from "@/components/Logo";
 import Link from "next/link";
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
+import axios from "axios";
 
 export default function Login() {
-    const router = useRouter()
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [error, setError] = useState<string | null>(null);
+
+    const loginSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        
+        try {
+            await axios.post("http://localhost:8080/auth/login", {
+                email: email,
+                senha: senha,
+            });
+            router.push('/home');
+        } 
+        catch (error: any) {
+            setError(error.data || "An error occurred during login.");
+        }
+    };
+
     return (
         <>
             <Link href="/"><Logo /></Link>
             
             <section className="login">
-                <form action="/login" method="post">
-                    <input type="email" name="email" id="email" placeholder="Email" required />
-                    <input type="password" name="senha" id="senha" placeholder="Senha" required />
-                    <input type="submit" value="Entrar" onClick={() => router.push('/home')}/>
+                <form onSubmit={loginSubmit}>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        placeholder="Email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        required 
+                    />
+                    <input 
+                        type="password" 
+                        name="senha" 
+                        id="senha" 
+                        placeholder="Senha" 
+                        value={senha} 
+                        onChange={(e) => setSenha(e.target.value)} 
+                        required 
+                    />
+                    <input type="submit" value="Entrar"/>
                 </form>
+                {error && <p className="error">{error}</p>}
             </section>
         </>
     );
