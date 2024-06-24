757268b9047636c138273ec385521d3702ca850b
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-06-23 23:29:50 -0300

    fix: Corrigido CSS e linkagem da página Filmes

--- 8-TheMovieDB(Em Dupla)/8-tmdb/src/app/filmes/page.tsx
+++ 8-TheMovieDB(Em Dupla)/8-tmdb/src/app/filmes/page.tsx
@@ -5,9 +5,17 @@ import "./filmes.css";
 import FilmeCard from "../components/filmeCard";
 import NavBar from "../components/navBar";
 
+interface Filme {
+    id: number;
+    poster_path: string;
+    release_date: string;
+    original_title: string;
+    overview: string
+}
+
 function Filmes(){
 
-    const [listaFilmes, setListaFilmes] = useState([])
+    const [listaFilmes, setListaFilmes] = useState<Filme[]>([])
 
     const getFilmes = () => {
         fetch("https://api.themoviedb.org/3/discover/movie?api_key=f5fafab7843ff239883cf22420e887df")
@@ -24,7 +32,7 @@ function Filmes(){
     return(
         <>
             <NavBar />
-            <div>
+            <div className="gradeFilmes">
                 {listaFilmes.map((filme)=>(
                     <FilmeCard
                     key={filme.id}
