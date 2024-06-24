de47dbef26de9e9aa5a321b8f0e7264141a28bea
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-06-24 16:25:35 -0300

    fix: Parcialmente implementado botão de favoritar

--- 8-TheMovieDB(Em Dupla)/8-tmdb/src/app/components/filmeCard.tsx
+++ 8-TheMovieDB(Em Dupla)/8-tmdb/src/app/components/filmeCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./filmeCard.css"
 
 interface FilmeCardProps {
@@ -8,7 +9,16 @@ interface FilmeCardProps {
 }
 
 const FilmeCard: React.FC<FilmeCardProps> = ({imagem, titulo, descricao, data}) => {
+    const [logado, setLogado] = useState<boolean>(false)
     
+
+    useEffect(()=>{
+        if (typeof window !== 'undefined') {
+            const token = localStorage.getItem('token')
+            setLogado(!!token)
+        }
+    }, [])
+
     return (
         <div className="filmeCard">
             <img src={`https://image.tmdb.org/t/p/w500${imagem}`} />
@@ -16,6 +26,9 @@ const FilmeCard: React.FC<FilmeCardProps> = ({imagem, titulo, descricao, data}) 
                 <h1>{titulo}</h1>
                 <h2>{data}</h2>
                 <p>{descricao}</p>
+                {logado && (
+                    <button>Favoritar</button>
+                )}
             </div>
         </div>
     )
