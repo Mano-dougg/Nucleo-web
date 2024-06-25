35ff7f9f475000b120dd1d92081b11bf9386d98b
Alexandre Coelho <alexandre.010c@gmail.com>
2024-06-25 01:49:34 -0300

    feat: main page + css

---
+++ 7-Lapadarie (Em Dupla)/src/components/Main.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import styles from "./Main.module.css";
+import InfoCard from "@/components/InfoCard"
+import SaleCard from "@/components/SaleCard"
+
+import PeopleIcon from "@/assets/People.svg"
+import CartIcon from "@/assets/Cart.svg"
+import DollarSignIcon from "@/assets/DollarSign.svg"
+import TrashIcon from "@/assets/Trash.svg"
+
+
+export default function Main() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.content}>
+        <section className={styles.information}>
+          <InfoCard Text={"Pessoas na fila"} Value={"7"} Icon={PeopleIcon} />
+          <InfoCard Text={"Pães vendidos"} Value={"350"} Icon={CartIcon} />
+          <InfoCard Text={"Entrada"} Value={"R$ 175,00"} Icon={DollarSignIcon} Highlight={true} />
+        </section>
+
+        <section className={styles.sales}>
+          <span className={styles.addPeople}>+ Adicionar pessoa a fila</span>
+          <div className={styles.salesList}>
+            <SaleCard Name={"Alexandre Shyjada Sousa"} Breads={50} Value={25} Icon={TrashIcon} />
+            <SaleCard Name={"Matheus Novais"} Breads={50} Value={25} Icon={TrashIcon} />
+            <SaleCard Name={"Victor Peixoto"} Breads={50} Value={25} Icon={TrashIcon} />
+            <SaleCard Name={"Kennedy Anderson"} Breads={50} Value={25} Icon={TrashIcon} />
+            <SaleCard Name={"João Victor"} Breads={50} Value={25} Icon={TrashIcon} />
+            <SaleCard Name={"Thales Brito"} Breads={50} Value={25} Icon={TrashIcon} />
+            <SaleCard Name={"Ivens Joris"} Breads={50} Value={25} Icon={TrashIcon} />
+          </div>
+        </section>
+
+        <div className={styles.footerMessage}>Com 💛 Info Jr UFBA 2022</div>
+      </div>
+    </main>
+  );
+}
---
+++ 7-Lapadarie (Em Dupla)/src/components/SaleCard.tsx
@@ -0,0 +1,30 @@
+import Image, { ImageProps } from "next/image";
+import styles from "./SaleCard.module.css";
+
+interface SaleCardProps {
+  Name: string;
+  Breads: number;
+  Value: number;
+  Icon: ImageProps['src'];
+}
+
+const SaleCard: React.FC<SaleCardProps> = ({Name, Breads, Value, Icon}) => {
+  const Price = new Intl.NumberFormat('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits:2
+  }).format(Value);
+
+  return (
+    <div className={styles.saleCard}>
+      <div className={styles.saleInformation}>
+        <div><span className={styles.bold}>{Name}</span></div>
+        <div><span className={styles.bold}>Total de pães:</span> {Breads} pães</div>
+        <div><span className={styles.bold}>Total a pagar:</span> R$ {Price}</div>
+      </div>
+
+      <Image className={styles.remove} src={Icon} alt="Remover" />
+    </div>
+  );
+}
+
+export default SaleCard;
