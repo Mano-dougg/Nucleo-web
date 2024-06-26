3769f46107f9baef00d89074986f8f88f62a1b59
EderNatan.dev <edernatan124@gmail.com>
2024-06-26 12:33:22 -0300

    Header+styles+function Getuser+logout

--- 8-TheMovieDB(Em Dupla)/movie/src/app/home/components2/header.tsx
+++ 8-TheMovieDB(Em Dupla)/movie/src/app/home/components2/header.tsx
@@ -1,12 +1,136 @@
-import Netflix from "../../../../img/netflix.jpg";
-import Image from "next/image";
+// app/components2/header.tsx
 
-export default function header() {
-    return (
-      <>
-      <div className="logoNetflix">
-        <Image src={Netflix} alt="Logo Netflix" />
-      </div>
-      </>
-    );
+import React from 'react';
+import Netflix from '../../../../img/netflix.jpg';
+import Image from 'next/image';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+interface HeaderProps {
+  user: {
+    name: string;
+    email: string;
+  } | null;
+}
+
+const Tagheader = styled.header`
+  font-size:20px;
+  position: sticky;
+  top: 0;
+  height: auto;
+  min-height: 70px;
+  z-index: 1;
+  background-color: #black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  font-family: 'Netflix Sans', 'Helvetica Neue', 'Segoe UI', 'Roboto', 'Ubuntu', sans-serif;
+  color: #fff; 
+ 
+
+  nav ul {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;
   }
+
+  nav li {
+    margin-right: 20px;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: #fff;
+    font-weight: bold;
+    transition: color 0.3s ease;
+  }
+
+  nav a:hover {
+    color: #e50914;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  p {
+    margin: 0 10px;
+    cursor: pointer;
+  }
+
+  .user-icon {
+    margin-right: 5px;
+  }
+
+`;
+
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const UserName = styled.p`
+  margin: 0 10px;
+  cursor: pointer;
+`;
+
+const Options = styled.div`
+  display: none;
+  top: calc(100% + 10px);
+  right: 0;
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  z-index: 10;
+
+  ${Tagheader}:hover & {
+    display: block;
+  }
+`;
+
+const Header: React.FC<HeaderProps> = ({ user }) => {
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    window.location.href = '/home';
+    window.location.reload(); 
+  };
+
+  const handleLogin = () => {
+    window.location.href = '/'; // 
+  };
+  
+  return (
+    <Tagheader>
+      <Image src={Netflix} alt="Netflix Logo" width={100} height={50} />
+      <nav>
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/">Series</a></li>
+          <li><a href="/">Filmes</a></li>
+          <li><a href="/">Minha Lista</a></li>
+        </ul>
+      </nav>
+      {user ? (
+        <UserContainer>
+          <FontAwesomeIcon icon={faUser} className="user-icon" />
+          <UserName>{user.name}</UserName>
+          <Options>
+            <p onClick={handleLogout} >logout</p>
+          </Options>
+        </UserContainer>
+      ) : (
+        <p onClick={handleLogin}>Login</p>
+      )}
+    </Tagheader>
+  );
+};
+
+export default Header;
--- 8-TheMovieDB(Em Dupla)/movie/src/app/home/page.tsx
+++ 8-TheMovieDB(Em Dupla)/movie/src/app/home/page.tsx
@@ -1,12 +1,60 @@
-"use client"
-import React from "react";
-import Header from './components2/header'
+// app/home.tsx
 
-export default function Home() {
-    return (
-      <>
-      <Header/>
-      </>
-    );
-  }
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Header from './components2/header';
+
+interface User {
+  name: string;
+  email: string;
   
+}
+
+const Home: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Função para buscar dados do usuário usando o token JWT do localStorage
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          throw new Error('Token JWT não encontrado.');
+        }
+
+        const response = await axios.get<User>('http://localhost:1080/profile', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+
+        setUser(response.data); // Armazena os dados do usuário no estado
+        setLoading(false); // Indica que a página carregou os dados do usuário
+      } catch (error) {
+        console.error('Erro ao buscar dados do usuário:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchUser(); 
+  }, []);
+
+  if (loading) {
+    return <p>Carregando...</p>; 
+  }
+
+  return (
+    <>
+      <Header user={user} />
+      <div>
+        <h1>Pagina Inicial!</h1>
+        
+      </div>
+    </>
+  );
+};
+
+export default Home;
