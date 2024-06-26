9baa6af7bdb008c118e26eb016feaa7e170dbb4b
unknown <rosabeatriz438@yahoo.com.br>
2024-06-26 20:18:04 -0300

    busca funcionando

--- 8-TheMovieDB(Em Dupla)/themovie-front/src/components/Pesquisados/pesquisados.tsx
+++ 8-TheMovieDB(Em Dupla)/themovie-front/src/components/Pesquisados/pesquisados.tsx
@@ -7,92 +7,99 @@ import Image from 'next/image';
 import like from '../../assets/coracao.png'
 import lupa from '../../assets/Lupa.png'
 
-
 interface testeFilme {
     id: number;
     title: string;
     poster_path: string;
 }
 
-const cadastrarFav = async (id: Key, path: string, title: string) => {
-
+const cadastrarFav = async (id: number, path: string, title: string) => {
     await axios.post("http://localhost:5000/favoritar", {
         userId: id,
         poster_path: path,
         title: title,
-      }); 
+    }); 
 
-      window.location.href = '/Favoritos';
-
+    window.location.href = '/Favoritos';
 }
 
-
 export default function Pesquisados() {
+    const [title, setTitle] = useState('');
+    const [dado, setDado] = useState<testeFilme | null>(null);
 
-    const [title, useTitle] = useState('')
-    const [dado, setDado] = useState <testeFilme | null>(null);
-
-
-    const enviar = () => {
-        if(title.trim()){
+    const enviar = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (title.trim()) {
             axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
-                    api_key:'04c35731a5ee918f014970082a0088b1',
+                    api_key: '04c35731a5ee918f014970082a0088b1',
                     query: title
                 }
-            }) 
-                .then((response) => {
-                    if (response.data.results && response.data.results.length > 0) {
-                        const resultado = response.data.results[0]
-                        alert(resultado.title)
-                        setDado({
-                            id: resultado.id,
-                            title: resultado.title,
-                            poster_path: resultado.poster_path
-                        })
-                        alert(dado?.title)
-                    } else {setDado(null);}
-                })
-                .catch((error) => {
+            })
+            .then((response) => {
+                if (response.data.results && response.data.results.length > 0) {
+                    const resultado = response.data.results[0];
+                    setDado({
+                        id: resultado.id,
+                        title: resultado.title,
+                        poster_path: resultado.poster_path
+                    });
+                    alert(dado?.title)
+                } else {
                     setDado(null);
-                  });
+                }
+            })
+            .catch((error) => {
+                setDado(null);
+            });
         }
     }
 
-    return(
-
+    return (
         <div className='div-mae-pesquisa'>
-                <div className='barra-pesquisa'>
-                    <form id='form-main'>
-                    <input type='text' 
-                    placeholder='Pesquisar filme'
-                    name='guardar'
-                    value={title}
-                    onChange={(e) => useTitle(e.target.value)}
+            <div className='barra-pesquisa'>
+                <form id='form-main' onSubmit={enviar}>
+                    <input 
+                        type='text' 
+                        placeholder='Pesquisar filme'
+                        name='guardar'
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
                     />
-                     <button onClick={()=>enviar()} type='submit' id='imagem-pesquisa'><Image src={lupa} alt=''/></button>
-                     </form>
-                    
-                </div>
+                    <button type='submit' id='imagem-pesquisa'>
+                        <Image src={lupa} alt='' />
+                    </button>
+                </form>
+            </div>
 
-
-
-                
-                {/* {dado && (                */}
-                <><div className='titulo-pesquisa'>
-                    <h2>Resultado da pesquisa:</h2>
-                </div><div className='div-pesquisa'>
+            {dado && (
+                <>
+                    <div className='titulo-pesquisa'>
+                        <h2>Resultado da pesquisa:</h2>
+                    </div>
+                    <div className='div-pesquisa'>
                         <div className="div-filme">
                             <div className="dentro-filme">
-                                <Image src={'https://image.tmdb.org/t/p/w500' + dado?.poster_path} alt="" width={150} height={201} />
-                                <p>{dado?.title}</p>
-                                <Image onClick={() => cadastrarFav(1, 'oi', 'oi')} src={like} alt="" width={25} height={25} id="like"></Image>
+                                <Image 
+                                    src={'https://image.tmdb.org/t/p/w500' + dado.poster_path} 
+                                    alt={""} 
+                                    width={150} 
+                                    height={201} 
+                                />
+                                <p>{dado.title}</p>
+                                <Image 
+                                    onClick={() => cadastrarFav(dado.id, dado.poster_path, dado.title)} 
+                                    src={like} 
+                                    alt="Like" 
+                                    width={25} 
+                                    height={25} 
+                                    id="like" 
+                                />
                             </div>
                         </div>
-                    </div></> 
-            {/* )} */}
-
+                    </div>
+                </>
+            )}
         </div>
-    )
-    
+    );
 }
