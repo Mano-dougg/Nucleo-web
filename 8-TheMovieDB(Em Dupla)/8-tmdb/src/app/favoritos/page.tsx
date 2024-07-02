f20ee58e39654170ad24d3772b5d4419f840b5a8
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-07-02 20:48:42 -0300

    fix: Corrigidos quantos filmes aparecem, ao invés de 20

--- 8-TheMovieDB(Em Dupla)/8-tmdb/src/app/favoritos/page.tsx
+++ 8-TheMovieDB(Em Dupla)/8-tmdb/src/app/favoritos/page.tsx
@@ -29,7 +29,7 @@ function Favoritos(){
     const getFilmes = () => {
         fetch("https://api.themoviedb.org/3/discover/movie?api_key=f5fafab7843ff239883cf22420e887df")
         .then(res => res.json())
-        .then(json => setListaFilmes(json.results))
+        .then(json => setListaFilmes(json.total_results))
     }
 
     useEffect(()=>{
--- 8-TheMovieDB(Em Dupla)/8-tmdb/src/app/filmes/page.tsx
+++ 8-TheMovieDB(Em Dupla)/8-tmdb/src/app/filmes/page.tsx
@@ -21,7 +21,7 @@ function Filmes(){
     const getFilmes = () => {
         fetch("https://api.themoviedb.org/3/discover/movie?api_key=f5fafab7843ff239883cf22420e887df")
         .then(res => res.json())
-        .then(json => setListaFilmes(json.results))
+        .then(json => setListaFilmes(json.total_results))
     }
 
     useEffect(()=>{
