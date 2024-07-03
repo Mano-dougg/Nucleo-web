0e8c3987afd4a38ccba41cdfbe79adef31d09d79
Fernando Costa <costa.fernando@ufba.br>
2024-07-03 18:03:04 -0300

    feat: adds dockerized server with user routes

---
+++ ecommerce-server/src/routes/userRoutes.ts
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import { Router } from 'express';
+// import { UserCreateInputSchema, UserPartialSchema } from '../../prisma/generated/zod/index';
+import {
+  // createUser,
+  deleteUser,
+  getUserByEmail,
+  // login
+} from '../controller/userController';
+// import { validateUser } from '../middleware/validateUser';
+
+const userRouter = Router();
+
+userRouter.use(cors());
+userRouter.delete('/', deleteUser);
+userRouter.get('/', getUserByEmail);
+// userRouter.post('/login', validateUser(UserPartialSchema), login);
+// userRouter.post('/signup', validateUser(UserCreateInputSchema), createUser);
+
+export default userRouter;
