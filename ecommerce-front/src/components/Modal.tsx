b67744bb8ced831d9ec3e942dd96a054bec5533a
Jaedson <jaedsonbarbosamacedo@gmail.com>
2024-07-09 20:07:27 -0300

    refactor: mudando e adicionando campos do modal para criação/edição

--- ecommerce-front/src/components/Modal.tsx
+++ ecommerce-front/src/components/Modal.tsx
@@ -12,29 +12,57 @@ import SizeSelect from "./SizeSelect";
 import Image from "next/image";
 import useDialog from "@/hooks/useDialog";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import deleteFn from "@/controlers/delete";
+import createFn from "@/controlers/create";
 
 const Modal = () => {
   const { isOpen, onClose, onOpen } = useDialog();
+  const [tag, setTag] = useState<string>("Selecionar Tags");
+  const [tags, setTags] = useState<string[]>([""]);
+  const [size, setSize] = useState<string>("Selecionar Tamanhos");
+  const [sizes, setSizes] = useState<string[]>([""]);
+
+  useEffect(() => {
+    if (tag.trim() !== "") {
+      if (tags.includes(tag)) {
+        return;
+      }
+
+      const newTags = [...tags, tag];
+      setTags(newTags);
+      setTag("");
+    }
+  }, [tag, tags]);
+
+  useEffect(() => {
+    if (size.trim() !== "") {
+      if (sizes.includes(size)) {
+        return;
+      }
+
+      const newSizes = [...sizes, size];
+      setSizes(newSizes);
+      setSize("");
+    }
+  }, [size]);
 
   return (
     <Dialog open={isOpen} modal defaultOpen={isOpen}>
       <DialogTrigger asChild>
-        <button
-          onClick={() => onOpen()}
-          className="sm:flex hidden"
-        >
+        <button onClick={() => onOpen()} className="sm:flex hidden">
           Edit Profile
         </button>
       </DialogTrigger>
       <Link href={"/modal"} className=" border border-black p-5 sm:hidden">
         Edit Profile
       </Link>
-      <DialogContent className="max-h-[800px] max-w-7xl w-full h-full flex flex-col items-center justify-center">
+      <DialogContent className="max-w-7xl w-full h-auto flex flex-col items-center justify-center">
         <DialogHeader>
           <DialogTitle className="text-center text-4xl">Editar</DialogTitle>
         </DialogHeader>
         <div
-          className={`max-w-5xl w-full h-3/4 flex items-center border border-black justify-center gap-16 px-2 overflow-hidden mb-14`}
+          className={`max-w-5xl w-full h-auto flex items-center border border-black justify-center gap-16 px-2 overflow-hidden mb-14`}
         >
           <Image
             src={"/Frame 153.png"}
@@ -42,7 +70,7 @@ const Modal = () => {
             width={414}
             alt="Imagem do produto"
           />
-          <form className="flex flex-col h-auto gap-4 " >
+          <form className="flex flex-col h-auto gap-4 py-3">
             <h3 className="modal_edit_form_title">Nome</h3>
             <input
               type="text"
@@ -85,12 +113,103 @@ const Modal = () => {
               />
             </div>
             <h3 className="modal_edit_form_title">Tamanhos</h3>
-            <div className="flex w-full justify-between  items-center ml-2 mb-5">
-              <SizeSelect peerName={"p"} value={"P"} />
-              <SizeSelect peerName={"m"} value={"M"} />
-              <SizeSelect peerName={"g"} value={"G"} />
-              <SizeSelect peerName={"gg"} value={"GG"} />
+            <div className="flex w-full flex-col justify-between  items-center ml-2 mb-5">
+              <select
+                name=""
+                id=""
+                className="modal_edit_form_input bg-bg-card mb-2"
+                value={size}
+                onChange={(e) =>
+                  createFn({ stateFn: setSize, item: e.target.value })
+                }
+              >
+                <option value="P">P</option>
+                <option value="M">M</option>
+                <option value="G">G</option>
+                <option value="GG">GG</option>
+              </select>
+              <div className="flex gap-2 flex-wrap font-bold">
+                {sizes?.map((item, index) =>
+                  item !== "" ? (
+                    <div
+                      key={index}
+                      className="bg-black text-white p-2 rounded-2xl"
+                    >
+                      {item}
+                      <span
+                        className="text-white font-bold ml-2 cursor-pointer"
+                        onClick={() =>
+                          deleteFn({ array: sizes, index, stateFn: setSizes })
+                        }
+                      >
+                        X
+                      </span>
+                    </div>
+                  ) : (
+                    ""
+                  )
+                )}
+              </div>
             </div>
+            <h3 className="modal_edit_form_title">Categorias</h3>
+            <select
+              className="modal_edit_form_input bg-bg-card"
+              title="tags"
+              onChange={(e) =>
+                createFn({ stateFn: setTag, item: e.target.value })
+              }
+              value={tag}
+            >
+              <option value="">Tags</option>
+              <optgroup title="categorias" label="Categorias">
+                <option value="Calças">Calças</option>
+                <option value="Blusas">Blusas</option>
+                <option value="Jaquetas">Jaquetas</option>
+                <option value="Vestidos">Vestidos</option>
+              </optgroup>
+              <optgroup title="categorias" label="Estilo">
+                <option value="Calças">Casual</option>
+                <option value="Blusas">Street</option>
+                <option value="Jaquetas">Sensual</option>
+                <option value="Vestidos">Social</option>
+              </optgroup>
+              <optgroup title="categorias" label="Caimento da peça">
+                <option value="Oversize">Oversize</option>
+                <option value="Skinny">Skinny</option>
+                <option value="Solto">Solto</option>
+                <option value="Slim Fit">Slim Fit</option>
+              </optgroup>
+            </select>
+            <div className="flex gap-2 flex-wrap font-bold">
+              {tags?.map((item, index) =>
+                item !== "" ? (
+                  <div
+                    key={index}
+                    className="bg-black text-white p-2 rounded-2xl"
+                  >
+                    {item}
+                    <span
+                      className="text-white font-bold ml-2 cursor-pointer"
+                      onClick={() =>
+                        deleteFn({ array: tags, index, stateFn: setTags })
+                      }
+                    >
+                      X
+                    </span>
+                  </div>
+                ) : (
+                  ""
+                )
+              )}
+            </div>
+            <h3 className="modal_edit_form_title">Quant</h3>
+            <input
+              type="number"
+              name=""
+              id=""
+              className="modal_edit_form_input"
+              placeholder="quant"
+            />
             <div className="flex justify-evenly">
               <button
                 type="submit"
